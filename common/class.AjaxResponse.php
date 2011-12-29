151f442c1582d44303c7ceb570042a4ac37e7471
calfonsi <calfonsi@ab00eea3-224b-49ca-824e-92964491e928>
2011-12-29 14:36:00 +0000

    Update ajax response, javascript signature style (assoc array)
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3062 ab00eea3-224b-49ca-824e-92964491e928

--- common/class.AjaxResponse.php
+++ common/class.AjaxResponse.php
@@ -2,16 +2,24 @@
 
 class common_AjaxResponse
 {
-    public function __construct($success=true, $type='json', $data=null, $message='')
+    public function __construct($options)
     {
+        $success    = isset($options['success'])    ?$options['success']    :true;
+        $type       = isset($options['type'])       ?$options['type']       :'json';
+        $data       = isset($options['data'])       ?$options['data']       :null;
+        $message    = isset($options['message'])    ?$options['message']    :'';
+        
+        //position the header of the response
         $context = Context::getInstance();
         $context->getResponse()->setContentHeader('text/json');
+        //set the response object
         $response = array(
             'success'           => $success
             , 'type'            => $type
             , 'message'         => $message
             , 'data'            => $data
         );
+        //write the response
         echo json_encode($response);
     }
 }
