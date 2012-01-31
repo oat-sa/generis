d37bf4a888b245055fd52c47a815acf504e6dc66
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2012-01-31 11:38:34 +0000

    Adding Compress Option to ArchiveLogger
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3085 ab00eea3-224b-49ca-824e-92964491e928

--- common/log/class.ArchiveFileAppender.php
+++ common/log/class.ArchiveFileAppender.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 09.12.2011, 11:59:31 with ArgoUML PHP module 
+ * Automatically generated on 31.01.2012, 12:07:23 with ArgoUML PHP module 
  * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
  * @author Joel Bout, <joel.bout@tudor.lu>
@@ -53,12 +53,36 @@ class common_log_ArchiveFileAppender
     // --- ATTRIBUTES ---
 
     /**
+     * Short description of attribute COMPRESSION_ZIP
+     *
+     * @access public
+     * @var string
+     */
+    const COMPRESSION_ZIP = 'zip';
+
+    /**
+     * Short description of attribute COMPRESSION_NONE
+     *
+     * @access public
+     * @var string
+     */
+    const COMPRESSION_NONE = 'none';
+
+    /**
      * Short description of attribute directory
      *
      * @access public
      * @var string
      */
     public $directory = '';
+
+    /**
+     * Short description of attribute compression
+     *
+     * @access public
+     * @var string
+     */
+    public $compression = 'zip';
 
     // --- OPERATIONS ---
 
@@ -76,18 +100,32 @@ class common_log_ArchiveFileAppender
 
         // section 127-0-1-1-7b882644:1342260c2b6:-8000:0000000000001866 begin
         if (isset($configuration['directory']) && $configuration['directory']) {
-        	$this->directory = $configuration['directory'];
+        	$this->directory = rtrim($configuration['directory'],DIRECTORY_SEPARATOR);
         } elseif (isset($configuration['file'])) {
         	$this->directory = dirname($configuration['file']);
+        }
+        if (isset($configuration['compression'])) {
+        	if (is_bool($configuration['compression'])) {
+        		$this->compression = $configuration['compression'] ? self::COMPRESSION_ZIP : self::COMPRESSION_NONE; 
+        	} else {
+        		switch ($configuration['compression']) {
+	        		case self::COMPRESSION_ZIP:
+	        			$this->compression = self::COMPRESSION_ZIP;
+	        			break;
+	        		case self::COMPRESSION_NONE:
+	        			$this->compression = self::COMPRESSION_NONE;
+	        			break;
+	        		default:
+	        			return false;
+	        	}
+        	}
         }
         
         if (!empty($this->directory))
         	$returnValue = parent::init($configuration);
         else
         	$returnValue = false;
-        // section 127-0-1-1-7b882644:1342260c2b6:-8000:0000000000001866 end
-        
-        return (bool) $returnValue;
+
         // section 127-0-1-1-7b882644:1342260c2b6:-8000:0000000000001866 end
 
         return (bool) $returnValue;
@@ -104,19 +142,61 @@ class common_log_ArchiveFileAppender
     {
         // section 127-0-1-1-7b882644:1342260c2b6:-8000:0000000000001868 begin
     	if ($this->maxFileSize > 0 && file_exists($this->filename) && filesize($this->filename) >= $this->maxFileSize) {
-	    	$filebase = basename($this->filename);
-	    	$dotpos = strrpos($filebase, ".");
-	    	$prefix = $this->directory.DIRECTORY_SEPARATOR.substr($filebase, 0, $dotpos)."_".date('Y-m-d');
-	    	$sufix = substr($filebase, $dotpos);
-	    	$count_string = "";
-	    	$count = 0;
-	    	while (file_exists($prefix.$count_string.$sufix)) {
-	    		$count_string = "_".++$count;
+	    	
+	    	if ($this->compression == self::COMPRESSION_ZIP) {
+	    		$zip = new ZipArchive;
+				$res = $zip->open($this->getAvailableArchiveFileName(), ZipArchive::CREATE);
+				if ($res === TRUE) {
+				    $zip->addFile($this->filename, basename($this->filename));
+				    $zip->close();
+				    unlink($this->filename);
+				} else {
+					//fail silently
+					return false;
+				}	
+	    	} elseif ($this->compression == self::COMPRESSION_NONE) {
+	    		$success = rename($this->filename, $this->getAvailableArchiveFileName());
+	    		if (!$success) {
+					//fail silently
+					return false;
+	    		}
+	    	} else {
+				//fail silently
+				return false;
 	    	}
-	    	rename($this->filename, $prefix.$count_string.$sufix);
     	}
     	$this->filehandle = @fopen($this->filename, 'a');
         // section 127-0-1-1-7b882644:1342260c2b6:-8000:0000000000001868 end
+    }
+
+    /**
+     * Short description of method getAvailableArchiveFileName
+     *
+     * @access private
+     * @author Joel Bout, <joel.bout@tudor.lu>
+     * @return string
+     */
+    private function getAvailableArchiveFileName()
+    {
+        $returnValue = (string) '';
+
+        // section 127-0-1-1-57e432c8:135336d8bea:-8000:0000000000004762 begin
+    	$filebase = basename($this->filename);
+    	$dotpos = strrpos($filebase, ".");
+    	if ($dotpos === false) {
+    		$dotpos = strlen($filebase);
+    	}
+    	$prefix = $this->directory.DIRECTORY_SEPARATOR.substr($filebase, 0, $dotpos)."_".date('Y-m-d');
+    	$sufix = substr($filebase, $dotpos).($this->compression === self::COMPRESSION_ZIP ? '.zip' : '');
+    	$count_string = "";
+    	$count = 0;
+    	while (file_exists($prefix.$count_string.$sufix)) {
+    		$count_string = "_".++$count;
+    	}
+        $returnValue = $prefix.$count_string.$sufix;
+        // section 127-0-1-1-57e432c8:135336d8bea:-8000:0000000000004762 end
+
+        return (string) $returnValue;
     }
 
 } /* end of class common_log_ArchiveFileAppender */
