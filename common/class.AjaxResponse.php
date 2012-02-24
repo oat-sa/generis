4e124e6ba0a1423b758e6e54097559e227d7de15
calfonsi <calfonsi@ab00eea3-224b-49ca-824e-92964491e928>
2012-02-24 16:46:57 +0000

    Update the AjaxReponse object (generate it with argo)
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3121 ab00eea3-224b-49ca-824e-92964491e928

--- common/class.AjaxResponse.php
+++ common/class.AjaxResponse.php
@@ -1,9 +1,61 @@
 <?php
 
+error_reporting(E_ALL);
+
+/**
+ * Generis Object Oriented API - common/class.AjaxResponse.php
+ *
+ * $Id$
+ *
+ * This file is part of Generis Object Oriented API.
+ *
+ * Automatically generated on 20.02.2012, 11:20:03 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
+ * @package common
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1E-includes begin
+// section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1E-includes end
+
+/* user defined constants */
+// section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1E-constants begin
+// section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1E-constants end
+
+/**
+ * Short description of class common_AjaxResponse
+ *
+ * @access public
+ * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
+ * @package common
+ */
 class common_AjaxResponse
 {
-    public function __construct($options)
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method __construct
+     *
+     * @access public
+     * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
+     * @param  array options
+     * @return mixed
+     */
+    public function __construct($options = array())
     {
+        // section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1F begin
+        
         $success    = isset($options['success'])    ?$options['success']    :true;
         $type       = isset($options['type'])       ?$options['type']       :'json';
         $data       = isset($options['data'])       ?$options['data']       :null;
@@ -19,8 +71,13 @@ class common_AjaxResponse
             , 'message'         => $message
             , 'data'            => $data
         );
+        
         //write the response
         echo json_encode($response);
+        
+        // section 127-0-1-1-641a0ff0:1359a3da29e:-8000:0000000000001B1F end
     }
-}
+
+} /* end of class common_AjaxResponse */
+
 ?>
