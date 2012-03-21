ac6a1861f5b885c7a55b7773ea772cff260c2e1a
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2012-03-21 11:12:00 +0000

    Skeletton of UriProviders.
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3147 ab00eea3-224b-49ca-824e-92964491e928

---
+++ common/uri/class.AbstractUriProvider.php
@@ -0,0 +1,114 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * Any implementation of the AbstractUriProvider class aims at providing unique
+ * to client code. It should take into account the state of the Knowledge Base
+ * avoid collisions. The AbstractUriProvider::provide method must be implemented
+ * subclasses to return a valid URI.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage uri
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001978-includes begin
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001978-includes end
+
+/* user defined constants */
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001978-constants begin
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001978-constants end
+
+/**
+ * Any implementation of the AbstractUriProvider class aims at providing unique
+ * to client code. It should take into account the state of the Knowledge Base
+ * avoid collisions. The AbstractUriProvider::provide method must be implemented
+ * subclasses to return a valid URI.
+ *
+ * @access public
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage uri
+ */
+class common_uri_AbstractUriProvider
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    /**
+     * The database driver the UriProvider is using.
+     *
+     * @access protected
+     * @var string
+     */
+    protected $driver = '';
+
+    // --- OPERATIONS ---
+
+    /**
+     * Returns the database driver that the UriProvider implementation is
+     * using.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return string
+     */
+    public function getDriver()
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:000000000000197C begin
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:000000000000197C end
+
+        return (string) $returnValue;
+    }
+
+    /**
+     * Sets the database driver that will be used for further URI generations.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string driver A driver name such as 'mysql', 'postgres', ...
+     * @return void
+     */
+    public function setDriver($driver)
+    {
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:000000000000197E begin
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:000000000000197E end
+    }
+
+    /**
+     * Provides a URI.
+     *
+     * @abstract
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return string
+     */
+    public abstract function provide();
+
+    /**
+     * Instantiates an instance of UriProvider for a given database driver.
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @param  string driver A driver name such as 'mysql', 'postgres', ...
+     * @return mixed
+     */
+    public function __construct($driver)
+    {
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001983 begin
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001983 end
+    }
+
+} /* end of class common_uri_AbstractUriProvider */
+
+?>
---
+++ common/uri/class.MicrotimeUriProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * UriProvider implementation based on PHP microtime.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage uri
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * Any implementation of the AbstractUriProvider class aims at providing unique
+ * to client code. It should take into account the state of the Knowledge Base
+ * avoid collisions. The AbstractUriProvider::provide method must be implemented
+ * subclasses to return a valid URI.
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ */
+require_once('common/uri/class.AbstractUriProvider.php');
+
+/* user defined includes */
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001993-includes begin
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001993-includes end
+
+/* user defined constants */
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001993-constants begin
+// section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001993-constants end
+
+/**
+ * UriProvider implementation based on PHP microtime.
+ *
+ * @access public
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage uri
+ */
+class common_uri_MicrotimeUriProvider
+    extends common_uri_AbstractUriProvider
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+    /**
+     * Generates a URI based on the value of PHP microtime().
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return string
+     */
+    public function provide()
+    {
+        $returnValue = (string) '';
+
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001995 begin
+        // section 10-13-1-85--341437fc:13634d84b3e:-8000:0000000000001995 end
+
+        return (string) $returnValue;
+    }
+
+} /* end of class common_uri_MicrotimeUriProvider */
+
+?>
