6ae47580f0d2c485d4375336a3cafee4d2d8d3ab
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2012-03-21 16:30:20 +0000

    add try catch in unit test that solve build problems
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3154 ab00eea3-224b-49ca-824e-92964491e928

--- test/hardify/UnhardifyTestCase.php
+++ test/hardify/UnhardifyTestCase.php
@@ -4,46 +4,50 @@ require_once INCLUDES_PATH.'/simpletest/autorun.php';
 
 
 class UnhardifyTestCase extends UnitTestCase {
-	
+
 	public function setUp(){
-	    TestRunner::initTest();
+		TestRunner::initTest();
 	}
-	
+
 	public function testHardify(){
-    	
+		 
 		ob_start(); // catch the output and drop it
-    	
-		$wfEngineHardifier = new wfEngine_scripts_HardifyWfEngine (array(
-			'min'		=> 1,
-			'required'	=> array(
-				array('compile'),
-				array('decompile')
-			),
-			'parameters' => array(
-				array(
-					'name' 			=> 'compile',
-					'type' 			=> 'boolean',
-					'shortcut'		=> 'c',
-					'required'		=> true,
-					'description'	=> 'Compile the workflow triple store to relational database'
-				),
-				array(
-					'name' 			=> 'decompile',
-					'type' 			=> 'boolean',
-					'shortcut'		=> 'd',
-					'required'		=> true,
-					'description'	=> 'Get the data from the workflow relational database to the triple store (if previously compiled)'
-				),
-				array(
-					'name'			=> 'indexes',
-					'type' 			=> 'boolean',
-					'shortcut'		=> 'i',
-					'description'	=> 'Create extra indexes on compiled tables and rebuild exisiting indexes databases'
-				)
-			)
-		), array ('argv'=>array('-d -i', '-d', '-i')));
-		
-    	ob_end_clean();
+		try {
+			$wfEngineHardifier = new wfEngine_scripts_HardifyWfEngine (array(
+					'min'		=> 1,
+					'required'	=> array(
+							array('compile'),
+							array('decompile')
+					),
+					'parameters' => array(
+							array(
+									'name' 			=> 'compile',
+									'type' 			=> 'boolean',
+									'shortcut'		=> 'c',
+									'required'		=> true,
+									'description'	=> 'Compile the workflow triple store to relational database'
+							),
+							array(
+									'name' 			=> 'decompile',
+									'type' 			=> 'boolean',
+									'shortcut'		=> 'd',
+									'required'		=> true,
+									'description'	=> 'Get the data from the workflow relational database to the triple store (if previously compiled)'
+							),
+							array(
+									'name'			=> 'indexes',
+									'type' 			=> 'boolean',
+									'shortcut'		=> 'i',
+									'description'	=> 'Create extra indexes on compiled tables and rebuild exisiting indexes databases'
+							)
+					)
+			), array ('argv'=>array('-d -i', '-d', '-i')));
+		}
+		catch (Exception $e){
+			var_dump($e);
+		}
+
+		ob_end_clean();
 		set_time_limit(900); // because the script update the time limit
 	}
 }
