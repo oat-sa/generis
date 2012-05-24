0e72286ae5c7599ae9d4ab041e00490372515352
jbihin <jbihin@ab00eea3-224b-49ca-824e-92964491e928>
2012-05-24 12:09:55 +0000

    improve boostrap for better handle of access test
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3209 ab00eea3-224b-49ca-824e-92964491e928

--- includes/ClearFw/core/AdvancedFC.class.php
+++ includes/ClearFw/core/AdvancedFC.class.php
@@ -2,8 +2,8 @@
 /**
  * AdvancedFC class
  * TODO AdvancedFC class documentation.
- * 
- * @author Jérôme Bogaerts <jerome.bogaerts@tudor.lu> <jerome.bogaerts@gmail.com>
+ *
+ * @author Jï¿½rï¿½me Bogaerts <jerome.bogaerts@tudor.lu> <jerome.bogaerts@gmail.com>
  */
 class AdvancedFC extends DefaultFC
 {
@@ -11,35 +11,31 @@ class AdvancedFC extends DefaultFC
 	{
 		parent::__construct($request);
 	}
-	
+
 	public function loadModule()
 	{
 		$resolver = new Resolver();
 		$action	= $resolver->getAction();
-    	$module	= $resolver->getModule();
-    	
-    	$context = Context::getInstance();
-    	$context->setModuleName($module);
-    	$context->setActionName($action);
-    	
-    	$enforcer = new ActionEnforcer();
-    	
-    	try
-    	{
-    		$enforcer->execute();
-    	}
-    	catch (InterruptedActionException $iE)
-    	{
-    		// Nothing to do here.
-    	}
+		$module	= $resolver->getModule();
+
+		$enforcer = new ActionEnforcer();
+
+		try
+		{
+			$enforcer->execute();
+		}
+		catch (InterruptedActionException $iE)
+		{
+			// Nothing to do here.
+		}
 	}
-	
+
 	public static function getView($pView)
 	{
 		throw new VersionException('FrontController::getView is deprecated since ' .
 								   'PHPFramework Evolution 1.');
 	}
-	
+
 	static function redirection($pRedirection = "", $pSauvegarde = true) {
 		throw new VersionException('FrontController:redirection is deprecated since ' .
 								   'PHPFramework Evolution 1.');
