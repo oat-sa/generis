149a803cc19c3f4e1c19274234ceb495346bb74c
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2012-07-19 14:35:32 +0000

    added common_configuration_BoundableComponent::getValue + implementation in child classes.
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3274 ab00eea3-224b-49ca-824e-92964491e928

--- common/configuration/class.BoundableComponent.php
+++ common/configuration/class.BoundableComponent.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 12.07.2012, 14:22:10 with ArgoUML PHP module 
+ * Automatically generated on 19.07.2012, 16:31:45 with ArgoUML PHP module 
  * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
  * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
@@ -156,6 +156,16 @@ abstract class common_configuration_BoundableComponent
 
         return (string) $returnValue;
     }
+
+    /**
+     * Short description of method getValue
+     *
+     * @abstract
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return string
+     */
+    public abstract function getValue();
 
 } /* end of abstract class common_configuration_BoundableComponent */
 
--- common/configuration/class.PHPExtension.php
+++ common/configuration/class.PHPExtension.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 12.07.2012, 14:22:10 with ArgoUML PHP module 
+ * Automatically generated on 19.07.2012, 16:31:45 with ArgoUML PHP module 
  * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
  * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
@@ -73,7 +73,7 @@ class common_configuration_PHPExtension
         $message = null;
         
         if (extension_loaded($name)){
-            $current = phpversion($name);
+            $current = $this->getValue();
             
             if (!empty($min) && !empty($max)){
                 // Both min and max are specified.
@@ -124,6 +124,24 @@ class common_configuration_PHPExtension
         // section -64--88-56-1--548fa03:1387a8a40e2:-8000:0000000000001ADB end
 
         return $returnValue;
+    }
+
+    /**
+     * Short description of method getValue
+     *
+     * @access public
+     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+     * @return string
+     */
+    public function getValue()
+    {
+        $returnValue = (string) '';
+
+        // section -64--88-56-1-1c6f58d0:1389fa4346a:-8000:0000000000001B30 begin
+        $returnValue = phpversion($this->getName());
+        // section -64--88-56-1-1c6f58d0:1389fa4346a:-8000:0000000000001B30 end
+
+        return (string) $returnValue;
     }
 
 } /* end of class common_configuration_PHPExtension */
