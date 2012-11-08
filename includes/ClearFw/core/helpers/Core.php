9f761cfeecd58b34c9fb8af7d91b5536a1b4fb21
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2012-11-08 12:03:50 +0000

    Core helpers documentation + set_data helper.
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3376 ab00eea3-224b-49ca-824e-92964491e928

--- includes/ClearFw/core/helpers/Core.php
+++ includes/ClearFw/core/helpers/Core.php
@@ -1,24 +1,55 @@
 <?php
+/**
+ * Load an helper file containing helper functions from the clearFw
+ * helpers core path defined by the DIR_CORE_HELPERS constant or from the current public helpers path
+ * defined by the DIR_HELPERS constant.
+ * 
+ * Helpers are loaded using the following procedure.
+ * - 1. Looks for a file [$helperName].php in DIR_CORE_HELPERS
+ * - 2. If it fails, looks for a file [$helperName].php in DIR_HELPERS
+ * - 3. If it fails, throws a HelperLoadingException.
+ *
+ * @param string $helperName The name of the helper to load.
+ */
 function load_helper($helperName)
 {
 	$corePath 		= DIR_CORE_HELPERS . $helperName . '.php';
 	$publicPath 	= DIR_HELPERS . $helperName . '.php';
 
-	if (file_exists($corePath))
+	if (file_exists($corePath)){
 		require_once($corePath);
-		
-	else if (file_exists($publicPath))
+	}	
+	else if (file_exists($publicPath)){
 		require_once($publicPath);
-	
+	}
 	else
 	{
 		throw new HelperLoadingException("The requested helper '${helperName}' could not be loaded.");
 	}
 }
 
+/**
+ * Get data from the request context.
+ *
+ * @param string $key A key to identify the data.
+ * @return mixed The data bound to the key. If no data is bound to the provided key, null is return.
+ */
 function get_data($key)
 {
 	$context = Context::getInstance();
 	return $context->getData($key);
+}
+
+/**
+ * Set data in the request context. If a data is associated with the key
+ * provided with the function call, it will be erased by the new one.
+ *
+ * @param string $key A key to identify the data.
+ * @param mixed $data The data to store, identified by the key.
+ * @return void
+ */
+function set_data($key, $data){
+	$context = Context::getInstance();
+	$context->setData($key, $data);
 }
 ?>
