1b70b7a390db43bab2984f3d61948381b9f3ad70
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2012-11-23 10:01:10 +0000

    common_configuration_ComponentFactory class added but to be tested.
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3407 ab00eea3-224b-49ca-824e-92964491e928

---
+++ common/configuration/class.ComponentFactoryException.php
@@ -0,0 +1,59 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * Generis Object Oriented API -
+ *
+ * $Id$
+ *
+ * This file is part of Generis Object Oriented API.
+ *
+ * Automatically generated on 23.11.2012, 10:20:54 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage configuration
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * include common_Exception
+ *
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ */
+require_once('common/class.Exception.php');
+
+/* user defined includes */
+// section 10-13-1-85-14a261be:13b2c72d816:-8000:0000000000001DC1-includes begin
+// section 10-13-1-85-14a261be:13b2c72d816:-8000:0000000000001DC1-includes end
+
+/* user defined constants */
+// section 10-13-1-85-14a261be:13b2c72d816:-8000:0000000000001DC1-constants begin
+// section 10-13-1-85-14a261be:13b2c72d816:-8000:0000000000001DC1-constants end
+
+/**
+ * Short description of class common_configuration_ComponentFactoryException
+ *
+ * @access public
+ * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
+ * @package common
+ * @subpackage configuration
+ */
+class common_configuration_ComponentFactoryException
+    extends common_Exception
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+} /* end of class common_configuration_ComponentFactoryException */
+
+?>
