7a137ab19baca948325b5c70df30742ad63412c7
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2012-12-04 14:49:57 +0000

    prevent a recursive svn_info for auth
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3415 ab00eea3-224b-49ca-824e-92964491e928

--- core/kernel/versioning/subversion/class.Repository.php
+++ core/kernel/versioning/subversion/class.Repository.php
@@ -127,7 +127,7 @@ class core_kernel_versioning_subversion_Repository
             svn_auth_set_parameter(SVN_AUTH_PARAM_DEFAULT_USERNAME, $login);
             svn_auth_set_parameter(SVN_AUTH_PARAM_DEFAULT_PASSWORD, $password);
 
-            if(@svn_info((string) $vcs->getOnePropertyValue(new core_kernel_classes_property(PROPERTY_GENERIS_VERSIONEDREPOSITORY_URL))) !== false){
+            if(@svn_info((string) $vcs->getOnePropertyValue(new core_kernel_classes_property(PROPERTY_GENERIS_VERSIONEDREPOSITORY_URL)), false) !== false){
                 $returnValue = true;
             }
         }
