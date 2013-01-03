d71b952b040df78596acfb9eaf27b804936373bf
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2013-01-03 09:43:57 +0000

    returnValue not returned in case of recursive class deletion.
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3463 ab00eea3-224b-49ca-824e-92964491e928

--- core/kernel/classes/class.Class.php
+++ core/kernel/classes/class.Class.php
@@ -542,7 +542,7 @@ class core_kernel_classes_Class
         $returnValue = (bool) false;
 
         // section 10-13-1-85-7ea8832b:13bfb40319c:-8000:0000000000001E63 begin
-        parent::delete($deleteReference);
+        $returnValue = parent::delete($deleteReference);
         // section 10-13-1-85-7ea8832b:13bfb40319c:-8000:0000000000001E63 end
 
         return (bool) $returnValue;
