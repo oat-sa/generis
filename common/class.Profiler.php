ee4b9054303580cd5e514c291129bc8755f3e7fc
ssipasseuth <ssipasseuth@ab00eea3-224b-49ca-824e-92964491e928>
2013-03-11 11:45:05 +0000

    common_Profiler : work in progress
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3572 ab00eea3-224b-49ca-824e-92964491e928

--- common/class.Profiler.php
+++ common/class.Profiler.php
@@ -1,10 +1,15 @@
 <?php
 class common_Profiler
 {
+	
 	private static $instance = null;
 	private $startTime = 0;
 	private $startTimeLogs = array();
 	private $elapsedTimeLogs = array();
+	private $slowQueries = array();
+	protected $loggers = array();//specify how messages should be logged, common_Logger, client popup
+	private $slowQueryTimer = 0;
+	private $slowQueryThreshold = 100;//in milliseconds (ms)
 	
 	public static function singleton()
     {
@@ -23,7 +28,7 @@ class common_Profiler
 		$this->startTime = self::getCurrentTime();
 	}
 	
-	private function getCurrentTime()
+	protected function getCurrentTime()
     {
 		return microtime(true);
 	}
@@ -35,13 +40,14 @@ class common_Profiler
 	
 	public function shutdownProfiler()
     {
-		$this->log('profiler shutting down');
+		$this->logRequestUrl();
 		$this->logTimer();
 		$this->logMemoryPeakUsage();
 		$this->logNrOfQueries();
+		$this->logSlowQueries();
 	}
 	
-	public function logTimer(){
+	protected function logTimer(){
 		$total = $this->getCurrentTime() - $this->startTime;
 		$totalRounded = round($total * 1000, 3);
 		$sumDurations = 0;
@@ -51,10 +57,10 @@ class common_Profiler
 				$sumDurations += $duration;
 		}
 		$uncovered = $total - $sumDurations;
-		$this->log('uncovered: ' . round($uncovered * 1000, 3) . 'ms/'.$totalRounded.'ms (' . round($uncovered / $total * 100, 1) . '%)');
+		$this->log('???: ' . round($uncovered * 1000, 3) . 'ms/'.$totalRounded.'ms (' . round($uncovered / $total * 100, 1) . '%)');
 	}
 	
-	private function prettyPrintMemory($mem){
+	protected function prettyPrintMemory($mem){
 		$returnValue = '';
 		if ($mem < 1024){
 			$returnValue = $mem.'B';
@@ -66,7 +72,7 @@ class common_Profiler
 		return $returnValue;
 	}
 	
-	public function logMemoryPeakUsage(){
+	protected function logMemoryPeakUsage(){
 		$memPeak = memory_get_peak_usage(true);
 		$memMax = ini_get('memory_limit');
 		$memPc = 0;
@@ -79,29 +85,127 @@ class common_Profiler
 		$this->log('peak mem usage: '.$this->prettyPrintMemory($memPeak).'/'.$this->prettyPrintMemory($memMax).' ('.$memPc.'%)');
 	}
 	
-	public function logNrOfQueries(){
+	protected function logNrOfQueries(){
 		$this->log(core_kernel_classes_DbWrapper::singleton()->getNrOfQueries().' queries for this request');
 	}
 	
-	private function log($msg){
+	protected function logRequestUrl(){
+		$requestUrl = Context::getInstance()->getExtensionName() . '/' . Context::getInstance()->getModuleName() . '/' . Context::getInstance()->getActionName();
+		$this->log('Profiling action called: '.$requestUrl);
+	}
+	protected function log($msg){
 		common_Logger::d($msg, array('PROFILER'));
 	}
 	
-	public static function start($eventKey){
-		self::singleton()->startTimer($eventKey);
+	public static function start($flag){
+		self::singleton()->startTimer($flag);
 	}
 	
-	public static function stop($eventKey){
-		self::singleton()->stopTimer($eventKey);
+	public static function stop($flag){
+		self::singleton()->stopTimer($flag);
 	}
 	
-	public function startTimer($eventKey = 'global'){
-		$this->startTimeLogs[$eventKey] = $this->getCurrentTime();
+	public static function queryStart(){
+		self::singleton()->startSlowQuery();
 	}
 	
-	public function stopTimer($eventKey = 'global'){
-		$startTime = isset($this->startTimeLogs[$eventKey])?$this->startTimeLogs[$eventKey]:$this->startTime;
-		$this->elapsedTimeLogs[$eventKey] = $this->getCurrentTime() - $startTime;
+	public static function queryStop($statement, $params = array()){
+		self::singleton()->stopSlowQuery($statement, $params);
 	}
+	
+	public function startTimer($flag = 'global'){
+		$this->startTimeLogs[$flag] = $this->getCurrentTime();
+	}
+	
+	public function stopTimer($flag = 'global'){
+		$startTime = isset($this->startTimeLogs[$flag])?$this->startTimeLogs[$flag]:$this->startTime;
+		$this->elapsedTimeLogs[$flag] = $this->getCurrentTime() - $startTime;
+	}
+	
+	public function startSlowQuery(){
+		$this->slowQueryTimer = $this->getCurrentTime();
+	}
+	
+	public function stopSlowQuery($statement, $params = array()){
+		if($this->slowQueryTimer){//log only if timer has been started
+			$statementKey = md5($statement);
+			$time = $this->getCurrentTime() - $this->slowQueryTimer;
+			if(1000*$time > $this->slowQueryThreshold){//compare to threshold (ms)
+				if(!isset($this->slowQueries[$statementKey])){
+					$this->slowQueries[$statementKey] =  array();
+				}
+				$this->slowQueries[$statementKey][] = array(
+					'statement' => $statement,
+					'params' => $params,
+					'time' => $time
+				);
+			}
+		}
+		$this->slowQueryTimer = 0;
+	}
+	
+	protected function logSlowQueries(){
+		foreach($this->slowQueries as $logs) {
+			$count = count($logs); 
+			for($i=0;$i<$count;$i++){
+				$this->log('slow query : '.$logs[$i]['statement'].' : '.round($logs[$i]['time']*1000, 3).'ms');
+			}
+		}
+	}
+	
+	/*
+	 * (experimental)
+	 * call common_Profiler::singleton()->initMysqlProfiler(); when after the DbWrapper has been constructed
+	 */
+	public function initMysqlProfiler(){
+		$dbWrapper = core_kernel_classes_DbWrapper::singleton();
+		if($dbWrapper instanceof core_kernel_classes_MysqlDbWrapper){
+			$dbWrapper->exec('SET profiling = 1');
+			$this->log('MySQL profiler enabled');
+		}
+	}
+
+	/*
+	 * (experimental)
+	 * log profiles from mysql built-in profiler
+	 */
+	protected function logMysqlProfiles(){
+		
+		$this->initMysqlProfiler();
+		
+		//get sql profile data
+		$profiles = array();
+		$profile = array();
+		$dbWrapper = core_kernel_classes_DbWrapper::singleton();
+		
+		$sqlProfilesResult = $dbWrapper->query('SHOW PROFILES');
+		while ($row = $sqlProfilesResult->fetch()) {
+			$id = $row['Query_ID'];
+
+			$sqlDataResult = $dbWrapper->query('SHOW PROFILE FOR QUERY ' . $id);
+			$profileData = array();
+			while ($r = $sqlDataResult->fetch()) {
+				$profileData[$r[0]] = $r;
+			}
+
+			$profiles[$id] = $profileData;
+			$profiles[$id]['duration'] = $row['Duration'];
+			$profiles[$id]['query'] = $row['Query'];
+		}
+
+		$sqlResult = $dbWrapper->query('SHOW PROFILE');
+		while ($row = $sqlResult->fetch()) {
+			$profile[$row[0]] = $row;
+		}
+
+		$report = array(
+			'queriesCount' => $dbWrapper->getNrOfQueries(),
+			'profile' => $profile,
+			'profiles' => $profiles
+		);
+		
+		$this->log(json_encode($report));
+	}
+	
 }
 ?>
