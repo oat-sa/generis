486ac4ac757f9911a85c143d36332e8b330c8c08
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2013-03-13 07:04:20 +0000

    add header to all files
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3578 ab00eea3-224b-49ca-824e-92964491e928

--- install/install.php
+++ install/install.php
@@ -1,4 +1,25 @@
 <?php
+/*  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
+ *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
+ * 
+ */
+?>
+<?php
 
 if (isset($_SERVER['CONFIG_PATH'])) {
 	define('CONFIG_PATH',$_SERVER['CONFIG_PATH']);
