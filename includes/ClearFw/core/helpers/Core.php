42c7ad1dcbf077d613f82e65ffb949c7a4d46bad
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2013-03-14 16:44:32 +0000

    * removed session taoqual * removed load_helper
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3585 ab00eea3-224b-49ca-824e-92964491e928

--- includes/ClearFw/core/helpers/Core.php
+++ includes/ClearFw/core/helpers/Core.php
@@ -21,35 +21,6 @@
 ?>
 <?php
 /**
- * Load an helper file containing helper functions from the clearFw
- * helpers core path defined by the DIR_CORE_HELPERS constant or from the current public helpers path
- * defined by the DIR_HELPERS constant.
- * 
- * Helpers are loaded using the following procedure.
- * - 1. Looks for a file [$helperName].php in DIR_CORE_HELPERS
- * - 2. If it fails, looks for a file [$helperName].php in DIR_HELPERS
- * - 3. If it fails, throws a HelperLoadingException.
- *
- * @param string $helperName The name of the helper to load.
- */
-function load_helper($helperName)
-{
-	$corePath 		= DIR_CORE_HELPERS . $helperName . '.php';
-	$publicPath 	= DIR_HELPERS . $helperName . '.php';
-
-	if (file_exists($corePath)){
-		require_once($corePath);
-	}	
-	else if (file_exists($publicPath)){
-		require_once($publicPath);
-	}
-	else
-	{
-		throw new HelperLoadingException("The requested helper '${helperName}' could not be loaded.");
-	}
-}
-
-/**
  * Get data from the request context.
  *
  * @param string $key A key to identify the data.
