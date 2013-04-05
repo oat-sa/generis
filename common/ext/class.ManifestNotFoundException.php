71bc65e0083c87b209301c747b54d59d19017d61
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2013-04-05 12:14:22 +0000

    escaladed severity of missing manifests
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3650 ab00eea3-224b-49ca-824e-92964491e928

--- common/ext/class.ManifestNotFoundException.php
+++ common/ext/class.ManifestNotFoundException.php
@@ -18,40 +18,9 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
 
 /**
- * This exception must be thrown when an Extension Manifest is requested but not
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- * @package common
- * @subpackage ext
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * An exception that occurs in the context of Extension Manifests.
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- */
-require_once('common/ext/class.ManifestException.php');
-
-/* user defined includes */
-// section -64--88-56-1-1a866557:1380f1ba529:-8000:0000000000001A5B-includes begin
-// section -64--88-56-1-1a866557:1380f1ba529:-8000:0000000000001A5B-includes end
-
-/* user defined constants */
-// section -64--88-56-1-1a866557:1380f1ba529:-8000:0000000000001A5B-constants begin
-// section -64--88-56-1-1a866557:1380f1ba529:-8000:0000000000001A5B-constants end
-
-/**
- * This exception must be thrown when an Extension Manifest is requested but not
+ * This exception must be thrown when an Extension Manifest is requested but not found
  *
  * @access public
  * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
@@ -68,6 +37,9 @@ class common_ext_ManifestNotFoundException
 
     // --- OPERATIONS ---
 
-} /* end of class common_ext_ManifestNotFoundException */
+	public function getSeverity() {
+		return common_Logger::WARNING_LEVEL;
+	}
+}
 
 ?>
