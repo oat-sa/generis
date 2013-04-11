080d110baf936ee1ecd8890f237a396fcd1cbb5b
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2013-04-11 15:53:00 +0000

    clean generis htaccess and remove old stuff
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3667 ab00eea3-224b-49ca-824e-92964491e928

--- index.php
+++ index.php
@@ -19,17 +19,5 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
 
-/**
- * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
- * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
- */
-require_once dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
-require_once dirname(__FILE__). '/common/common.php';
-
-$bootStrap = new BootStrap('generis');
-$bootStrap->start();
-$bootStrap->dispatch();
 ?>
