70c40be31c85ce4989f93af80e36bbb80793247a
pplichart <pplichart@ab00eea3-224b-49ca-824e-92964491e928>
2013-06-06 13:47:20 +0000

    work in progress (rest ) , exception handler
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3713 ab00eea3-224b-49ca-824e-92964491e928

--- common/exception/class.PreConditionFailure.php
+++ common/exception/class.PreConditionFailure.php
@@ -16,24 +16,43 @@
  *
  *
  */
+/**
+ * Generis Object Oriented API - common/exception/class.InvalidArgumentType.php
+ *
+ * $Id$
+ *
+ * This file is part of Generis Object Oriented API.
+ *
+ * Automatically generated on 30.01.2012, 16:44:05 with ArgoUML PHP module
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author patrick,
+ * @package common
+ * @subpackage exception
+ */
 
 
 
+/* user defined includes */
+// section 127-0-1-1--7d7a54ea:134896cda52:-8000:00000000000044FA-includes begin
+// section 127-0-1-1--7d7a54ea:134896cda52:-8000:00000000000044FA-includes end
+
+/* user defined constants */
+// section 127-0-1-1--7d7a54ea:134896cda52:-8000:00000000000044FA-constants begin
+// section 127-0-1-1--7d7a54ea:134896cda52:-8000:00000000000044FA-constants end
 
 /**
- * 
+ * a useful exception
  * @access public
  * @author Patrick Plichart
  * @package common
  * @subpackage exception
  */
-abstract class common_exception_BadRequest
+class common_exception_PreConditionFailure
     extends common_exception_ClientException
 {
-    // --- ASSOCIATIONS ---
-    // --- ATTRIBUTES ---
   
-
+   
 } /* end of class common_exception_InvalidArgumentType */
 
 ?>
