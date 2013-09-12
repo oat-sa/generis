008c66381db9ff2b1b7a5c9db30b5e5aa2e155a2
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2013-09-12 08:05:18 +0000

    changed severity to warnign to facilitate debgugging
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3772 ab00eea3-224b-49ca-824e-92964491e928

--- common/exception/class.EmptyProperty.php
+++ common/exception/class.EmptyProperty.php
@@ -19,50 +19,13 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
 
 /**
- * Generis Object Oriented API - common/exception/class.EmptyProperty.php
- *
- * $Id$
- *
- * This file is part of Generis Object Oriented API.
- *
- * Automatically generated on 16.04.2012, 11:13:26 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- * @package common
- * @subpackage exception
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * include common_Exception
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('common/class.Exception.php');
-
-/* user defined includes */
-// section 127-0-1-1--4a441dc4:135c963290f:-8000:0000000000001960-includes begin
-// section 127-0-1-1--4a441dc4:135c963290f:-8000:0000000000001960-includes end
-
-/* user defined constants */
-// section 127-0-1-1--4a441dc4:135c963290f:-8000:0000000000001960-constants begin
-// section 127-0-1-1--4a441dc4:135c963290f:-8000:0000000000001960-constants end
-
-/**
- * Short description of class common_exception_EmptyProperty
+ * Thrown when retrieving an expected property
+ * and not finding it (usualy via getUniqueProperty()) 
  *
  * @access public
- * @author Joel Bout, <joel.bout@tudor.lu>
+ * @author Joel Bout, <joel@taotesting.com>
  * @package common
  * @subpackage exception
  */
@@ -75,18 +38,14 @@ class common_exception_EmptyProperty
     // --- ATTRIBUTES ---
 
     /**
-     * Short description of attribute property
-     *
      * @access private
-     * @var Property
+     * @var core_kernel_classes_Property
      */
     private $property = null;
 
     /**
-     * Short description of attribute resource
-     *
      * @access private
-     * @var Resource
+     * @var core_kenel_classes_Resource
      */
     private $resource = null;
 
@@ -96,57 +55,48 @@ class common_exception_EmptyProperty
      * Short description of method __construct
      *
      * @access public
-     * @author Joel Bout, <joel.bout@tudor.lu>
-     * @param  Resource resource
-     * @param  Property property
+     * @param  core_kernel_classes_Resource resource
+     * @param  core_kernel_classes_Property property
      * @return mixed
      */
     public function __construct( core_kernel_classes_Resource $resource,  core_kernel_classes_Property $property)
     {
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019C8 begin
         $this->resource = $resource;
         $this->property = $property;
         parent::__construct("Property {$property->getLabel()} ({$property->getUri()}) of resource {$resource->getLabel()} ({$resource->getUri()})
             							 should not be empty");
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019C8 end
     }
 
     /**
-     * Short description of method getProperty
+     * Returns the property that was empty
      *
      * @access public
-     * @author Joel Bout, <joel.bout@tudor.lu>
      * @return core_kernel_classes_Property
      */
     public function getProperty()
     {
-        $returnValue = null;
-
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019CF begin
-        $returnValue = $this->property;
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019CF end
-
-        return $returnValue;
+        return $this->property;
     }
 
     /**
-     * Short description of method getResource
+     * Returns the resource with the empty property
      *
      * @access public
-     * @author Joel Bout, <joel.bout@tudor.lu>
      * @return core_kernel_classes_Resource
      */
     public function getResource()
     {
-        $returnValue = null;
-
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019D1 begin
-        $returnValue = $this->resource;
-        // section 127-0-1-1-6b4920b6:136ba654e76:-8000:00000000000019D1 end
-
-        return $returnValue;
+        return $this->resource;
     }
 
-} /* end of class common_exception_EmptyProperty */
+    /**
+     * Returns the severity of the exception
+     * Used in the common/Logger
+     * 
+     * @return number
+     */
+    public function getSeverity() {
+        return common_Logger::WARNING_LEVEL;
+    }
 
-?>
+}
