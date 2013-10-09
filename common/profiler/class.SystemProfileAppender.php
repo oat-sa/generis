56dbdd1b67fc4e4199814dcfd0f623fad5800056
ssipasseuth <ssipasseuth@ab00eea3-224b-49ca-824e-92964491e928>
2013-10-09 10:18:06 +0000

    catch exception in archiver configuration
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3800 ab00eea3-224b-49ca-824e-92964491e928

--- common/profiler/class.SystemProfileAppender.php
+++ common/profiler/class.SystemProfileAppender.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/*
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -25,14 +25,13 @@
  * @package common
  * @subpackage log
  */
-class common_profiler_SystemProfileAppender
-        extends common_profiler_Appender
+class common_profiler_SystemProfileAppender extends common_profiler_Appender
 {
 
-	protected $comment = '';
-	protected $data = array();
-	protected $archivers = array();
-	 
+    protected $comment = '';
+    protected $data = array();
+    protected $archivers = array();
+
     /**
      * Short description of method init
      *
@@ -42,110 +41,111 @@ class common_profiler_SystemProfileAppender
      * @return boolean
      */
     public function init($configuration){
-		
-        $returnValue = (bool) false;
-    	
-    	parent::init($configuration);
-		$this->data = array();
-		$this->archivers = array();
-		
-		if(isset($configuration['local_server_comment'])){
-			$this->comment = strval($configuration['local_server_comment']);
-		}
-		
-		foreach ($configuration['archivers'] as $archiverConfig){
-    		if(isset($archiverConfig['class'])){
-    			$classname = $archiverConfig['class'];
-    			if (!class_exists($classname)){
-    				$classname = 'common_profiler_archiver_'.$classname;
-                }
-    			if (class_exists($classname)){
-    				$archiver = new $classname();
-    				if ($archiver instanceof common_profiler_archiver_Archiver && !is_null($archiver) && $archiver->init($archiverConfig)) {
-						$this->archivers[] = $archiver;
-					}
-				}
-			}
-		}	
-		
-    	$returnValue = true;
 
-        return (bool) $returnValue;
+        parent::init($configuration);
+        $this->data = array();
+        $this->archivers = array();
+
+        if(isset($configuration['local_server_comment'])){
+            $this->comment = strval($configuration['local_server_comment']);
+        }
+
+        foreach($configuration['archivers'] as $archiverConfig){
+            if(isset($archiverConfig['class'])){
+                $classname = $archiverConfig['class'];
+                if(!class_exists($classname)){
+                    $classname = 'common_profiler_archiver_'.$classname;
+                }
+                if(class_exists($classname)){
+                    $archiver = new $classname();
+                    try{
+                        if($archiver instanceof common_profiler_archiver_Archiver && !is_null($archiver) && $archiver->init($archiverConfig)){
+                            $this->archivers[] = $archiver;
+                        }
+                    }catch(InvalidArgumentException $e){
+                        common_Logger::w('archiver configuration issue: '.$e->getMessage());
+                    }
+                }
+            }
+        }
+
+        return true;
     }
-	
-	public function logContext(common_profiler_Context $context){
-		$this->data['context'] = $context->toArray();
-		if(!empty($this->comment)){
-			$this->data['context']['comment'] = $this->comment;
-		}
-	}
-	
-	public function logTimer($flag, $duration, $total){
-		if(!isset($this->data['timer'])){
-			$this->data['timer'] = array();
-		}
-		if(!isset($this->data['timer'][$flag])){
-			$this->data['timer'][$flag] = array();
-		}
-		if(!isset($this->data['timer']['TOTAL'])){
-			$this->data['timer']['TOTAL'] = $total;
-		}
-		$this->data['timer'][$flag][] = $duration;
-	}
-	
-	public function logMemoryPeak($memPeak, $memMax){
-		if(!isset($this->data['mem'])){
-			$this->data['mem'] = array();
-		}
-		$this->data['mem']['peak'] = $memPeak;
-		$this->data['mem']['max'] = $memMax;
-	}
-	
-	public function logQueriesCount($count){
-		if(!isset($this->data['query'])){
-			$this->data['query'] = array();
-		}
-		$this->data['query']['count'] = $count;
-	}
-	
-	public function logQueriesSlow($slowQueries){
-		if(!isset($this->data['query'])){
-			$this->data['query'] = array();
-		}
-	
-		$logs = array();
-		foreach($slowQueries as $statementKey => $queries){
-			$count = count($queries);
-			$logs[$statementKey] = array();
-			for($i=0; $i<$count; $i++){
-				$logs[$statementKey] = $queries[$i]->toArray();
-			}
-		}
-	
-		$this->data['query']['slow'] = $logs;
-	}
-	
-	public function logQueriesSlowest($slowestQueries){
-		if(!isset($this->data['query'])){
-			$this->data['query'] = array();
-		}
-		$this->data['query']['slowest'] = array();
-		foreach($slowestQueries as $query){
-			$this->data['query']['slowest'][] = $query->toArray();
-		}
-	}
-	
-	public function logQueriesStat($queries){
-		$count = $this->getConfigOption('queries', 'count');
-		if(!is_null($count)){
-			$this->data['query']['stat'] = $queries;
-		}
-	}
-	
-	public function flush(){
-		foreach($this->archivers as $archiver){
-			$archiver->store($this->data);
-		}
-	}
-	
-} /* end of class common_profiler_SystemAppender */
+
+    public function logContext(common_profiler_Context $context){
+        $this->data['context'] = $context->toArray();
+        if(!empty($this->comment)){
+            $this->data['context']['comment'] = $this->comment;
+        }
+    }
+
+    public function logTimer($flag, $duration, $total){
+        if(!isset($this->data['timer'])){
+            $this->data['timer'] = array();
+        }
+        if(!isset($this->data['timer'][$flag])){
+            $this->data['timer'][$flag] = array();
+        }
+        if(!isset($this->data['timer']['TOTAL'])){
+            $this->data['timer']['TOTAL'] = $total;
+        }
+        $this->data['timer'][$flag][] = $duration;
+    }
+
+    public function logMemoryPeak($memPeak, $memMax){
+        if(!isset($this->data['mem'])){
+            $this->data['mem'] = array();
+        }
+        $this->data['mem']['peak'] = $memPeak;
+        $this->data['mem']['max'] = $memMax;
+    }
+
+    public function logQueriesCount($count){
+        if(!isset($this->data['query'])){
+            $this->data['query'] = array();
+        }
+        $this->data['query']['count'] = $count;
+    }
+
+    public function logQueriesSlow($slowQueries){
+        if(!isset($this->data['query'])){
+            $this->data['query'] = array();
+        }
+
+        $logs = array();
+        foreach($slowQueries as $statementKey => $queries){
+            $count = count($queries);
+            $logs[$statementKey] = array();
+            for($i = 0; $i < $count; $i++){
+                $logs[$statementKey] = $queries[$i]->toArray();
+            }
+        }
+
+        $this->data['query']['slow'] = $logs;
+    }
+
+    public function logQueriesSlowest($slowestQueries){
+        if(!isset($this->data['query'])){
+            $this->data['query'] = array();
+        }
+        $this->data['query']['slowest'] = array();
+        foreach($slowestQueries as $query){
+            $this->data['query']['slowest'][] = $query->toArray();
+        }
+    }
+
+    public function logQueriesStat($queries){
+        $count = $this->getConfigOption('queries', 'count');
+        if(!is_null($count)){
+            $this->data['query']['stat'] = $queries;
+        }
+    }
+
+    public function flush(){
+        foreach($this->archivers as $archiver){
+            $archiver->store($this->data);
+        }
+    }
+
+}
+/* end of class common_profiler_SystemAppender */
