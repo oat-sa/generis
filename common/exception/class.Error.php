fbe3196b9110d980de47530bcd7dada5e24ff26f
jbogaerts <jbogaerts@ab00eea3-224b-49ca-824e-92964491e928>
2014-01-30 11:43:27 +0000

    doc
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3938 ab00eea3-224b-49ca-824e-92964491e928

--- common/exception/class.Error.php
+++ common/exception/class.Error.php
@@ -19,51 +19,6 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-
-error_reporting(E_ALL);
-
-/**
- * Generis Object Oriented API - common/exception/class.Error.php
- *
- * $Id$
- *
- * This file is part of Generis Object Oriented API.
- *
- * Automatically generated on 24.04.2012, 18:04:53 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- * @package common
- * @subpackage exception
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * include common_Exception
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('common/class.Exception.php');
-
-/**
- * include common_log_SeverityLevel
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('common/log/interface.SeverityLevel.php');
-
-/* user defined includes */
-// section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001905-includes begin
-// section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001905-includes end
-
-/* user defined constants */
-// section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001905-constants begin
-// section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001905-constants end
 
 /**
  * Short description of class common_exception_Error
@@ -77,15 +32,9 @@ class common_exception_Error
     extends common_Exception
         implements common_log_SeverityLevel
 {
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
-     * Short description of method getSeverity
+     * Get the severity of the error.
      *
      * @access public
      * @author Joel Bout, <joel.bout@tudor.lu>
@@ -95,13 +44,10 @@ class common_exception_Error
     {
         $returnValue = (int) 0;
 
-        // section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001907 begin
         $returnValue = common_Logger::ERROR_LEVEL;
-        // section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001907 end
 
         return (int) $returnValue;
     }
-
-} /* end of class common_exception_Error */
+}
 
 ?>
