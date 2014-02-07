d87fdaec526ffd3b23e29f6c6777a12e50ba7cd1
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2014-02-07 10:23:23 +0000

    fixed infinite loop on missing file access
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3946 ab00eea3-224b-49ca-824e-92964491e928

--- helpers/class.PhpTools.php
+++ helpers/class.PhpTools.php
@@ -19,35 +19,37 @@ class helpers_PhpTools {
         $fp = fopen($file, 'r');
         $class = $namespace = $buffer = '';
         $i = 0;
-        while (!$class && !feof($fp)) {
-        
-            $buffer .= fread($fp, 512);
-            // supress errors due to partially parsing
-            $tokens = @token_get_all($buffer);
-        
-            if (strpos($buffer, '{') === false) {
-                continue;
-            }
-        
-            for (;$i<count($tokens);$i++) {
-                if ($tokens[$i][0] === T_NAMESPACE) {
-                    for ($j=$i+1;$j<count($tokens); $j++) {
-                        if ($tokens[$j][0] === T_STRING) {
-                             $namespace .= '\\'.$tokens[$j][1];
-                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
-                             break;
+        if ($fp !== false) {
+            while (!$class && !feof($fp)) {
+            
+                $buffer .= fread($fp, 512);
+                // supress errors due to partially parsing
+                $tokens = @token_get_all($buffer);
+            
+                if (strpos($buffer, '{') === false) {
+                    continue;
+                }
+            
+                for (;$i<count($tokens);$i++) {
+                    if ($tokens[$i][0] === T_NAMESPACE) {
+                        for ($j=$i+1;$j<count($tokens); $j++) {
+                            if ($tokens[$j][0] === T_STRING) {
+                                 $namespace .= '\\'.$tokens[$j][1];
+                            } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                                 break;
+                            }
                         }
                     }
-                }
-        
-                if ($tokens[$i][0] === T_CLASS) {
-                    for ($j=$i+1;$j<count($tokens);$j++) {
-                        if ($tokens[$j] === '{') {
-                            if (!isset($tokens[$i+2][1])) {
-                                common_Logger::i($file.' does not contain a valid class definition');
-                                break;
-                            } else { 
-                                $class = $tokens[$i+2][1];
+            
+                    if ($tokens[$i][0] === T_CLASS) {
+                        for ($j=$i+1;$j<count($tokens);$j++) {
+                            if ($tokens[$j] === '{') {
+                                if (!isset($tokens[$i+2][1])) {
+                                    common_Logger::i($file.' does not contain a valid class definition');
+                                    break;
+                                } else { 
+                                    $class = $tokens[$i+2][1];
+                                }
                             }
                         }
                     }
