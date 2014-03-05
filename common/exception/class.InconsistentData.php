225e94485a6ec6a268a0ae1166adc76790e6a7f5
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2014-03-05 08:10:37 +0000

    clean code style
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3984 ab00eea3-224b-49ca-824e-92964491e928

--- common/exception/class.InconsistentData.php
+++ common/exception/class.InconsistentData.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -19,51 +19,7 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
 
-error_reporting(E_ALL);
-
-/**
- * Generis Object Oriented API - common/exception/class.InconsistentData.php
- *
- * $Id$
- *
- * This file is part of Generis Object Oriented API.
- *
- * Automatically generated on 24.04.2012, 18:06:19 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- * @package common
- * @subpackage exception
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * include common_Exception
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('common/class.Exception.php');
-
-/**
- * include common_log_SeverityLevel
- *
- * @author Joel Bout, <joel.bout@tudor.lu>
- */
-require_once('common/log/interface.SeverityLevel.php');
-
-/* user defined includes */
-// section 127-0-1-1-118e77b:136e5136795:-8000:0000000000001D9C-includes begin
-// section 127-0-1-1-118e77b:136e5136795:-8000:0000000000001D9C-includes end
-
-/* user defined constants */
-// section 127-0-1-1-118e77b:136e5136795:-8000:0000000000001D9C-constants begin
-// section 127-0-1-1-118e77b:136e5136795:-8000:0000000000001D9C-constants end
 
 /**
  * Short description of class common_exception_InconsistentData
@@ -77,12 +33,7 @@ class common_exception_InconsistentData
     extends common_Exception
         implements common_log_SeverityLevel
 {
-    // --- ASSOCIATIONS ---
 
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
      * Short description of method getSeverity
@@ -93,15 +44,9 @@ class common_exception_InconsistentData
      */
     public function getSeverity()
     {
-        $returnValue = (int) 0;
-
-        // section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001907 begin
-        $returnValue = common_Logger::ERROR_LEVEL;
-        // section 127-0-1-1--15445bbd:1352f3a7eb2:-8000:0000000000001907 end
-
-        return (int) $returnValue;
+        return common_Logger::ERROR_LEVEL;
     }
 
-} /* end of class common_exception_InconsistentData */
+} 
 
 ?>
