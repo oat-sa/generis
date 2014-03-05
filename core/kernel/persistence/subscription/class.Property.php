3c45951e935494c9e2115ef66a25d0f668fcef83
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2014-03-05 08:16:53 +0000

    clean code style
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@3985 ab00eea3-224b-49ca-824e-92964491e928

--- core/kernel/persistence/subscription/class.Property.php
+++ core/kernel/persistence/subscription/class.Property.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -18,51 +18,7 @@
  *               
  * 
  */
-?>
-<?php
 
-error_reporting(E_ALL);
-
-/**
- * Generis Object Oriented API -
- *
- * $Id$
- *
- * This file is part of Generis Object Oriented API.
- *
- * Automatically generated on 27.12.2012, 14:55:59 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- * @package core
- * @subpackage kernel_persistence_subscription
- */
-
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
-}
-
-/**
- * include core_kernel_persistence_PersistenceImpl
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- */
-require_once('core/kernel/persistence/class.PersistenceImpl.php');
-
-/**
- * include core_kernel_persistence_PropertyInterface
- *
- * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
- */
-require_once('core/kernel/persistence/interface.PropertyInterface.php');
-
-/* user defined includes */
-// section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000140C-includes begin
-// section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000140C-includes end
-
-/* user defined constants */
-// section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000140C-constants begin
-// section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000140C-constants end
 
 /**
  * Short description of class core_kernel_persistence_subscription_Property
@@ -76,10 +32,7 @@ class core_kernel_persistence_subscription_Property
     extends core_kernel_persistence_PersistenceImpl
         implements core_kernel_persistence_PropertyInterface
 {
-    // --- ASSOCIATIONS ---
 
-
-    // --- ATTRIBUTES ---
 
     /**
      * Short description of attribute instance
@@ -102,13 +55,7 @@ class core_kernel_persistence_subscription_Property
      */
     public function getSubProperties( core_kernel_classes_Resource $resource, $recursive = false)
     {
-        $returnValue = array();
-
-        // section 127-0-1-1-7b8668ff:12f77d22c39:-8000:000000000000144D begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 127-0-1-1-7b8668ff:12f77d22c39:-8000:000000000000144D end
-
-        return (array) $returnValue;
     }
 
     /**
@@ -121,13 +68,7 @@ class core_kernel_persistence_subscription_Property
      */
     public function isLgDependent( core_kernel_classes_Resource $resource)
     {
-        $returnValue = (bool) false;
-
-        // section 127-0-1-1--bedeb7e:12fb15494a5:-8000:00000000000014DB begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 127-0-1-1--bedeb7e:12fb15494a5:-8000:00000000000014DB end
-
-        return (bool) $returnValue;
     }
 
     /**
@@ -140,13 +81,7 @@ class core_kernel_persistence_subscription_Property
      */
     public function isMultiple( core_kernel_classes_Resource $resource)
     {
-        $returnValue = (bool) false;
-
-        // section 127-0-1-1--bedeb7e:12fb15494a5:-8000:00000000000014DD begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 127-0-1-1--bedeb7e:12fb15494a5:-8000:00000000000014DD end
-
-        return (bool) $returnValue;
     }
 
     /**
@@ -161,9 +96,7 @@ class core_kernel_persistence_subscription_Property
     {
         $returnValue = null;
 
-        // section 127-0-1-1-7a0c731b:12fbfab7535:-8000:0000000000001539 begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 127-0-1-1-7a0c731b:12fbfab7535:-8000:0000000000001539 end
 
         return $returnValue;
     }
@@ -179,15 +112,7 @@ class core_kernel_persistence_subscription_Property
      */
     public function delete( core_kernel_classes_Resource $resource, $deleteReference = false)
     {
-        $returnValue = (bool) false;
-
-        // section 127-0-1-1--330ca9de:1318ac7ca9f:-8000:0000000000001641 begin
-        
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        
-        // section 127-0-1-1--330ca9de:1318ac7ca9f:-8000:0000000000001641 end
-
-        return (bool) $returnValue;
     }
 
     /**
@@ -201,13 +126,8 @@ class core_kernel_persistence_subscription_Property
      */
     public function setRange( core_kernel_classes_Resource $resource,  core_kernel_classes_Class $class)
     {
-        $returnValue = null;
-
-        // section 10-13-1-85-36aaae10:13bad44a267:-8000:0000000000001E25 begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 10-13-1-85-36aaae10:13bad44a267:-8000:0000000000001E25 end
 
-        return $returnValue;
     }
 
     /**
@@ -221,9 +141,8 @@ class core_kernel_persistence_subscription_Property
      */
     public function setMultiple( core_kernel_classes_Resource $resource, $isMultiple)
     {
-        // section 10-13-1-85-71dc1cdd:13bade8452c:-8000:0000000000001E32 begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 10-13-1-85-71dc1cdd:13bade8452c:-8000:0000000000001E32 end
+
     }
 
     /**
@@ -237,9 +156,7 @@ class core_kernel_persistence_subscription_Property
      */
     public function setLgDependent( core_kernel_classes_Resource $resource, $isLgDependent)
     {
-        // section 10-13-1-85-4a20f448:13bdca46e9a:-8000:0000000000001E45 begin
         throw new core_kernel_persistence_ProhibitedFunctionException("not implemented => The function (".__METHOD__.") is not available in this persistence implementation (".__CLASS__.")");
-        // section 10-13-1-85-4a20f448:13bdca46e9a:-8000:0000000000001E45 end
     }
 
     /**
@@ -253,14 +170,12 @@ class core_kernel_persistence_subscription_Property
     {
         $returnValue = null;
 
-        // section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000142B begin
         
         if (core_kernel_persistence_subscription_Property::$instance == null){
         	core_kernel_persistence_subscription_Property::$instance = new core_kernel_persistence_subscription_Property();
         }
         $returnValue = core_kernel_persistence_subscription_Property::$instance;
         
-        // section 127-0-1-1--499759bc:12f72c12020:-8000:000000000000142B end
 
         return $returnValue;
     }
@@ -277,12 +192,10 @@ class core_kernel_persistence_subscription_Property
     {
         $returnValue = (bool) false;
 
-        // section 127-0-1-1--499759bc:12f72c12020:-8000:0000000000001431 begin
-        // section 127-0-1-1--499759bc:12f72c12020:-8000:0000000000001431 end
 
         return (bool) $returnValue;
     }
 
-} /* end of class core_kernel_persistence_subscription_Property */
+} 
 
 ?>
