c9f6549d657ce929fb09e727069ac8b4a591e680
jbout <jbout@ab00eea3-224b-49ca-824e-92964491e928>
2014-03-21 08:20:52 +0000

    fixed bug in getClassInfo for classes containing the character '{' before the class definition
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@4010 ab00eea3-224b-49ca-824e-92964491e928

--- helpers/class.PhpTools.php
+++ helpers/class.PhpTools.php
@@ -9,48 +9,35 @@ class helpers_PhpTools {
     /**
      * Generate a random alphanumeric token with a specific $length.
      * 
-     * Code based on http://stackoverflow.com/questions/7153000/get-class-name-from-file
+     * Code losely based on http://stackoverflow.com/questions/7153000/get-class-name-from-file
      * by user http://stackoverflow.com/users/492901/netcoder
      * 
      * @param string file to anaylse
      * @return array
      */
     static public function getClassInfo($file) {
-        $fp = fopen($file, 'r');
-        $class = $namespace = $buffer = '';
-        $i = 0;
-        if ($fp !== false) {
-            while (!$class && !feof($fp)) {
-            
-                $buffer .= fread($fp, 512);
-                // supress errors due to partially parsing
-                $tokens = @token_get_all($buffer);
-            
-                if (strpos($buffer, '{') === false) {
-                    continue;
-                }
-            
-                for (;$i<count($tokens);$i++) {
-                    if ($tokens[$i][0] === T_NAMESPACE) {
-                        for ($j=$i+1;$j<count($tokens); $j++) {
-                            if ($tokens[$j][0] === T_STRING) {
-                                 $namespace .= '\\'.$tokens[$j][1];
-                            } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
-                                 break;
-                            }
-                        }
+	    $buffer = file_get_contents($file);
+	    $tokens = @token_get_all($buffer);
+	    $class = $namespace = $buffer = '';
+        for ($i=0;$i<count($tokens);$i++) {
+            if ($tokens[$i][0] === T_NAMESPACE) {
+                for ($j=$i+1;$j<count($tokens); $j++) {
+                    if ($tokens[$j][0] === T_STRING) {
+                        $namespace .= '\\'.$tokens[$j][1];
+                    } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                        break;
                     }
-            
-                    if ($tokens[$i][0] === T_CLASS) {
-                        for ($j=$i+1;$j<count($tokens);$j++) {
-                            if ($tokens[$j] === '{') {
-                                if (!isset($tokens[$i+2][1])) {
-                                    common_Logger::i($file.' does not contain a valid class definition');
-                                    break;
-                                } else { 
-                                    $class = $tokens[$i+2][1];
-                                }
-                            }
+                }
+            }
+
+            if ($tokens[$i][0] === T_CLASS) {
+                for ($j=$i+1;$j<count($tokens);$j++) {
+                    if ($tokens[$j] === '{') {
+                        if (!isset($tokens[$i+2][1])) {
+                            error_log($file.' does not contain a valid class definition');
+                            break;
+                        } else {
+                            $class = $tokens[$i+2][1];
                         }
                     }
                 }
