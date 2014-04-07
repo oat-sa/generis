8dd337d7a34b6977ba20002a7be69808bfce12b9
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2014-04-07 07:43:45 +0000

    parse error
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@4022 ab00eea3-224b-49ca-824e-92964491e928

--- common/exception/class.InvalidArgumentType.php
+++ common/exception/class.InvalidArgumentType.php
@@ -19,7 +19,7 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?
+
 /**
  * Short description of class common_exception_InvalidArgumentType
  *
