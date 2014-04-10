e7b5f76ae70a505491ab79ce7cb211837d981ef8
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2014-04-10 14:33:07 +0000

    phpdoc
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@4036 ab00eea3-224b-49ca-824e-92964491e928

--- common/persistence/sql/dbal/class.Driver.php
+++ common/persistence/sql/dbal/class.Driver.php
@@ -15,7 +15,11 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
+ */
+
+/**
+ * Dbal Driver 
+ * 
  * @author Lionel Lecaque  <lionel@taotesting.com>
  * @license GPLv2
  * @package generis
@@ -24,9 +28,15 @@
 class common_persistence_sql_dbal_Driver implements common_persistence_sql_Driver
 {
 
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
     protected $connection;
 
     /**
+     * Connect to Dbal
+     * 
+     * @param string $id
      * @param array $params
      * @return \Doctrine\DBAL\Connection;
      */
@@ -44,14 +54,16 @@ class common_persistence_sql_dbal_Driver implements common_persistence_sql_Drive
     
    
 
-    /* (non-PHPdoc)
+    /**
+     * (non-PHPdoc)
      * @see common_persistence_sql_Driver::getPlatForm()
      */
     public function getPlatForm(){
         return new common_persistence_sql_Platform($this->connection->getDatabasePlatform());
     }
     
-    /* (non-PHPdoc)
+    /**
+     * (non-PHPdoc)
      * @see common_persistence_sql_Driver::getSchemaManager()
      */
     public function getSchemaManager()
@@ -61,9 +73,12 @@ class common_persistence_sql_dbal_Driver implements common_persistence_sql_Drive
     
    
     /**
-     * @access
+     * Execute the statement with provided params
+     *
      * @author "Lionel Lecaque, <lionel@taotesting.com>"
      * @param mixed $statement
+     * @param array $params
+     * @return integer number of affected row
      */
     public function exec($statement,$params = array())
     {
@@ -72,9 +87,11 @@ class common_persistence_sql_dbal_Driver implements common_persistence_sql_Drive
     
     
     /**
-     * @access
+     * Query  the statement with provided params
+     * 
      * @author "Lionel Lecaque, <lionel@taotesting.com>"
      * @param mixed $statement
+     * @return \Doctrine\DBAL\Driver\Statement
      */
     public function query($statement,$params = array())
     {
