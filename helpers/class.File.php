db4d0e62b42d3ae81bb36efe4dc7429b786cbac9
llecaque <llecaque@ab00eea3-224b-49ca-824e-92964491e928>
2014-05-23 10:14:37 +0000

    fix checkstyle
    git-svn-id: http://vcs.taotesting.com/svn/generis/branches/GenerisMinimalApi@4080 ab00eea3-224b-49ca-824e-92964491e928

--- helpers/class.File.php
+++ helpers/class.File.php
@@ -374,7 +374,9 @@ class helpers_File
         $parts     = array_filter( explode( DIRECTORY_SEPARATOR, $path ), 'strlen' );
         $absolutes = array();
         foreach ( $parts as $part ) {
-            if ( '.' == $part ) continue;
+            if ( '.' == $part ) {
+                continue;
+            }
             if ( '..' == $part ) {
                 array_pop( $absolutes );
             } else {
@@ -383,7 +385,9 @@ class helpers_File
         }
         $path = implode( DIRECTORY_SEPARATOR, $absolutes );
         // resolve any symlinks
-        if ( file_exists( $path ) && linkinfo( $path ) > 0 ) $path = readlink( $path );
+        if ( file_exists( $path ) && linkinfo( $path ) > 0 ) {
+            $path = readlink( $path );
+        }
         // put initial separator that could have been lost
         $path = !$unipath ? '/' . $path : $path;
         return $path;
