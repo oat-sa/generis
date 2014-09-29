c1a6f1e36346bed4b9038bfaa092024ea1101cc0
Lionel Lecaque <lionel@taotesting.com>
2014-09-29 11:53:32 +0200

    add test namesapce in composer remove include inc.extension.php

--- test/GenerisPhpUnitTestRunner.php
+++ test/GenerisPhpUnitTestRunner.php
@@ -20,7 +20,7 @@
  */
 ini_set('max_execution_time', 0);
 ini_set('memory_limit', '-1');
-require_once dirname(__FILE__).'/../common/inc.extension.php';
+//require_once dirname(__FILE__).'/../common/inc.extension.php';
 
 
 /**
