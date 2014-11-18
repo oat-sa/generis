7a30bda026df07056d343f77c712c8de9ff4265d
bugalood <jerome@taotesting.com>
2014-11-18 13:48:42 +0100

    A little bit of cleaning in smoothsql utils.

--- core/kernel/persistence/smoothsql/class.Utils.php
+++ core/kernel/persistence/smoothsql/class.Utils.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -17,68 +17,60 @@
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ *               2012-2014 (update and modification) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 /**
- * Short description of class core_kernel_persistence_smoothsql_Utils
- *
- * @access public
- * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
- * @package generis
- 
+ * Utility class for package core\kernel\persistence\smoothsql.
+ * 
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ * @author Cédric Alfonsi <cerdic.alfonsi@tudor.lu>
  */
 class core_kernel_persistence_smoothsql_Utils
 {
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
 
     /**
-     * Short description of method sortByLanguage
+     * Sort a given $dataset by language.
      *
-     * @access public
-     * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
-     * @param  dataset
-     * @param  string langColname
-     * @return array
+     * @param mixed dataset A PDO dataset.
+     * @param string langColname The name of the column corresponding to the language of results.
+     * @return array An array representing the sorted $dataset.
      */
     public static function sortByLanguage($dataset, $langColname)
     {
         $returnValue = array();
-
         
         $dbWrapper = core_kernel_classes_DbWrapper::singleton();
         $session = core_kernel_classes_Session::singleton(); 
-    	$selectedLanguage = $session->getDataLanguage();
-    	$defaultLanguage = DEFAULT_LANG;
-    	$fallbackLanguage = '';
-    					  
-    	$sortedResults = array($selectedLanguage => array(),
-    						   $defaultLanguage => array(),
-    						   $fallbackLanguage => array());
-    					  
-    	foreach ($dataset as $row) {
-    		$sortedResults[$row[$langColname]][] = array('value' => $dbWrapper->getPlatForm()->getPhpTextValue($row['object']), 
-    													 'language' => $row[$langColname]);
-    	}
-    	
-    	$returnValue = array_merge($sortedResults[$selectedLanguage], 
-    							   (count($sortedResults) > 2) ? $sortedResults[$defaultLanguage] : array(),
-    							   $sortedResults[$fallbackLanguage]);
-        
+        $selectedLanguage = $session->getDataLanguage();
+        $defaultLanguage = DEFAULT_LANG;
+        $fallbackLanguage = '';
+        				  
+        $sortedResults = array(
+            $selectedLanguage => array(),
+            $defaultLanguage => array(),
+            $fallbackLanguage => array()
+        );
 
-        return (array) $returnValue;
+        foreach ($dataset as $row) {
+        	$sortedResults[$row[$langColname]][] = array(
+        	    'value' => $dbWrapper->getPlatForm()->getPhpTextValue($row['object']), 
+        	    'language' => $row[$langColname]
+            );
+        }
+        
+        $returnValue = array_merge(
+            $sortedResults[$selectedLanguage], 
+            (count($sortedResults) > 2) ? $sortedResults[$defaultLanguage] : array(),
+            $sortedResults[$fallbackLanguage]
+        );
+        
+        return $returnValue;
     }
 
     /**
-     * Short description of method getFirstLanguage
+     * Get the first language encountered in the $values associative array.
      *
-     * @access public
-     * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
      * @param  array values
      * @return array
      */
@@ -86,43 +78,36 @@ class core_kernel_persistence_smoothsql_Utils
     {
         $returnValue = array();
 
+        if (count($values) > 0) {
+            $previousLanguage = $values[0]['language'];
         
-   		if (count($values) > 0) {
-    		$previousLanguage = $values[0]['language'];
-
-    		foreach ($values as $value) {
-    			if ($value['language'] == $previousLanguage) {
-    				$returnValue[] = $value['value'];
-    			}
-    			else {
-    				break;
-    			}
-    		}
-    	}
-        
+            foreach ($values as $value) {
+                if ($value['language'] == $previousLanguage) {
+                    $returnValue[] = $value['value'];
+                } else {
+                    break;
+                }
+            }
+        }
 
         return (array) $returnValue;
     }
 
     /**
-     * Short description of method filterByLanguage
+     * Filter a $dataset by language.
      *
-     * @access public
-     * @author Cédric Alfonsi, <cedric.alfonsi@tudor.lu>
-     * @param  dataset
-     * @param  string langColname
+     * @param mixed dataset
+     * @param string langColname
      * @return array
      */
     public static function filterByLanguage($dataset, $langColname)
     {
         $returnValue = array();
-
         
         $result = self::sortByLanguage($dataset, $langColname);
-    	$returnValue = self::getFirstLanguage($result);
+        $returnValue = self::getFirstLanguage($result);
         
-
-        return (array) $returnValue;
+        return $returnValue;
     }
 
     /**
@@ -135,77 +120,71 @@ class core_kernel_persistence_smoothsql_Utils
      */
     public static function identifyFirstLanguage($values)
     {
-        $returnValue = (string) '';
+        $returnValue = '';
 
-        
-    	if (count($values) > 0) {
-    		$previousLanguage = $values[0]['language'];
-    		$returnValue = $previousLanguage;
-    		
-    		foreach ($values as $value) {
-    			if ($value['language'] == $previousLanguage) {
-    				continue;
-    			}
-    			else {
-    				$returnValue = $previousLanguage;
-    				break;
-    			}
-    		}
-    	}
-        
+        if (count($values) > 0) {
+            $previousLanguage = $values[0]['language'];
+            $returnValue = $previousLanguage;
+            
+            foreach ($values as $value) {
+                if ($value['language'] == $previousLanguage) {
+                    continue;
+                } else {
+                    $returnValue = $previousLanguage;
+                    break;
+                }
+            }
+        }
 
-        return (string) $returnValue;
+        return $returnValue;
     }
 
     /**
      * Build a SQL search pattern on basis of a pattern and a comparison mode.
      *
-     * @access public
-     * @author Jerome Bogaerts, <jerome.bogaerts@tudor.lu>
-     * @param  string pattern A value to compare.
+     * @param  tring pattern A value to compare.
      * @param  boolean like The manner to compare values. If set to true, the LIKE SQL operator will be used. If set to false, the = (equal) SQL operator will be used.
      * @return string
      */
     public static function buildSearchPattern($pattern, $like = true)
     {
-        $returnValue = (string) '';
-    
+        $returnValue = '';
+        
         $dbWrapper = core_kernel_classes_DbWrapper::singleton();
-    
+        
         switch (gettype($pattern)) {
-        	case 'string' :
-        	case 'numeric':
-        	    $patternToken = $pattern;
-        	    $object = trim(str_replace('*', '%', $patternToken));
-    
-        	    if($like){
-        	        if(!preg_match("/^%/", $object)){
-        	            $object = "%" . $object;
-        	        }
-        	        if(!preg_match("/%$/", $object)){
-        	            $object = $object . "%";
-        	        }
-        	        $returnValue .= ' LIKE '. $dbWrapper->quote($object);
-        	    }
-        	    else {
-        	        $returnValue = (strpos($object, '%') !== false)
-        	        ? 'LIKE '. $dbWrapper->quote($object)
-        	        : '= '. $dbWrapper->quote($patternToken);
-        	    }
-        	    break;
-    
-        	case 'object' :
-        	    if($pattern instanceof core_kernel_classes_Resource) {
-        	        $returnValue = ' = ' . $dbWrapper->quote($pattern->getUri());
-        	    } else {
-        	        common_Logger::w('non ressource as search parameter: '. get_class($pattern), 'GENERIS');
-        	    }
-        	    break;
-    
-        	default:
-        	    throw new common_Exception("Unsupported type for searchinstance array: " . gettype($value));
+            case 'string' :
+            case 'numeric':
+                $patternToken = $pattern;
+                $object = trim(str_replace('*', '%', $patternToken));
+                
+                if ($like){
+                    if (!preg_match("/^%/", $object)) {
+                        $object = "%" . $object;
+                    }
+                    if (!preg_match("/%$/", $object)) {
+                        $object = $object . "%";
+                    }
+                    $returnValue .= ' LIKE '. $dbWrapper->quote($object);
+                } else {
+                    $returnValue = (strpos($object, '%') !== false)
+                    ? 'LIKE '. $dbWrapper->quote($object)
+                    : '= '. $dbWrapper->quote($patternToken);
+                }
+                break;
+            
+            case 'object' :
+                if ($pattern instanceof core_kernel_classes_Resource) {
+                    $returnValue = ' = ' . $dbWrapper->quote($pattern->getUri());
+                } else {
+                    common_Logger::w('non ressource as search parameter: '. get_class($pattern), 'GENERIS');
+                }
+                break;
+            
+            default:
+                throw new common_Exception("Unsupported type for searchinstance array: " . gettype($value));
         }
-    
-        return (string) $returnValue;
+        
+        return $returnValue;
     }
 }
