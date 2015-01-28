e9f04c5e59ff72d4661fe62f1bf396d57b0b60f6
Lionel Lecaque <lionel@taotesting.com>
2015-01-28 20:34:15 +0100

    add sample and provider to check getModelId

--- test/model/persistence/file/FileModelTest.php
+++ test/model/persistence/file/FileModelTest.php
@@ -1,23 +1,22 @@
 <?php
 /**
  * This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; under version 2
-* of the License (non-upgradable).
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*
-* Copyright (c) (original work) 2015 Open Assessment Technologies SA
-*
-*/
-
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) (original work) 2015 Open Assessment Technologies SA
+ *
+ */
 namespace oat\generis\test\model\kernel\persistence\file;
 
 use oat\generis\test\GenerisPhpUnitTestRunner;
@@ -26,6 +25,7 @@ use \common_exception_MissingParameter;
 
 class FileModelTest extends GenerisPhpUnitTestRunner
 {
+
     /**
      *
      * @see PHPUnit_Framework_TestCase::setUp()
@@ -34,7 +34,7 @@ class FileModelTest extends GenerisPhpUnitTestRunner
     {
         GenerisPhpUnitTestRunner::initTest();
     }
-    
+
     /**
      *
      * @author Lionel Lecaque, lionel@taotesting.com
@@ -54,7 +54,7 @@ class FileModelTest extends GenerisPhpUnitTestRunner
         $model = new FileModel($conf);
         return $model;
     }
-    
+
     /**
      * @depends testConstruct
      *
@@ -67,37 +67,70 @@ class FileModelTest extends GenerisPhpUnitTestRunner
         ), $model->getConfig());
     }
 
-    
     /**
      * @depends testConstruct
      *
      * @author Lionel Lecaque, lionel@taotesting.com
-     * @param array $model
+     * @param array $model            
      */
     public function testGetRdfInterface($model)
     {
         $this->assertInstanceOf('oat\generis\model\kernel\persistence\file\FileRdf', $model->getRdfInterface());
     }
-    
+
     /**
      * @depends testConstruct
      * @expectedException common_exception_NoImplementation
+     * 
      * @author Lionel Lecaque, lionel@taotesting.com
-     * @param array $model
+     * @param array $model            
      */
     public function testGetRdfsInterface($model)
     {
         $model->getRdfsInterface();
     }
-    
+
     /**
+     *
+     * @author Lionel Lecaque, lionel@taotesting.com
+     * @return array
+     */
+    public function modelProvider()
+    {
+        $dir = GenerisPhpUnitTestRunner::getSampleDir();
+        return array(
+            array(
+                6,
+                $dir . '/rdf/generis.rdf'
+            ),
+            array(
+                4,
+                $dir . '/rdf/widget.rdf'
+            ),
+            array(
+                100,
+                $dir . '/rdf/nobase.rdf'
+            )
+        );
+    }
+
+    /**
+     * @dataProvider modelProvider
      * 
      * @author Lionel Lecaque, lionel@taotesting.com
      */
-    public function testGetModelIdFromXml(){
-        
+    public function testGetModelIdFromXml($id, $file)
+    {
+        try {
+            $modelid = FileModel::getModelIdFromXml($file);
+            $this->assertEquals($id, $modelid);
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('\common_exception_Error', $e);
+            if ($id == 100) {
+                $this->assertEquals('The namespace of /home/lionel/work/php/package-tao/generis/test/samples/rdf/nobase.rdf has to be defined with the "xml:base" attribute of the ROOT node', $e->getMessage());
+            }
+        }
     }
-    
 }
 
 ?>
