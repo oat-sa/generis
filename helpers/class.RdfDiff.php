c5f2a791c7bfbe606a8e06de34d0d7c97f33d858
Joel Bout <joel@taotesting.com>
2015-01-30 10:06:06 +0100

    Added helpers for extension model synchronisation

---
+++ helpers/class.RdfDiff.php
@@ -0,0 +1,99 @@
+<?php
+
+class helpers_RdfDiff
+{
+    private $added = array();
+
+    private $removed = array();
+
+    private $toAdd = array();
+
+    private $toRemove = array();
+
+    /**
+     * Creates a diff from a set of triples to a set of troples
+     * 
+     * 
+     * @param Traversable $from
+     * @param Traversable $to
+     */
+    public static function create(Traversable $from, Traversable $to) {
+        $diff = new self();
+        
+        foreach ($to as $triple) {
+            $diff->add($triple);
+        }
+        
+        
+        foreach ($from as $triple) {
+            $diff->remove($triple);
+        }
+        return $diff;
+    }
+
+    public function __construct()
+    {
+
+    }
+
+    public function add(core_kernel_classes_Triple $triple)
+    {
+        $serial = $this->generateSerial($triple);
+        $this->added[$serial] = true;
+        if (! isset($this->removed[$serial])) {
+            $this->toAdd[$serial] = $triple;
+        } elseif (isset($this->toRemove[$serial])) {
+            unset($this->toRemove[$serial]);
+        }
+    }
+
+    public function remove(core_kernel_classes_Triple $triple)
+    {
+        $serial = $this->generateSerial($triple);
+        $this->removed[$serial] = true;
+        if (! isset($this->added[$serial])) {
+            $this->toRemove[$serial] = $triple;
+        } elseif (isset($this->toAdd[$serial])) {
+            unset($this->toAdd[$serial]);
+        }
+    }
+    
+    protected function generateSerial(core_kernel_classes_Triple $triple) {
+        return md5(implode(' ', array($triple->subject, $triple->predicate, $triple->object, $triple->lg, $triple->modelid)));
+    }
+    
+    protected function shorten(core_kernel_classes_Triple $triple) {
+        return array(
+            's' => $triple->subject,
+            'p' => $triple->predicate,
+            'o' => $triple->object,
+            'l' => is_null($triple->lg) ? '' : $triple->lg,
+            'm' => $triple->modelid
+        );
+    }
+    
+    public function getSummary() {
+        return count($this->toAdd).' triples to add and '.count($this->toRemove).' triples to remove';
+    }
+    
+    public function dump() {
+        foreach ($this->toAdd as $triple) {
+            echo '+ '.str_pad($triple->subject, 80).' '.str_pad($triple->predicate, 80).' '.str_pad($triple->object, 80).PHP_EOL;
+        }
+        foreach ($this->toRemove as $triple) {
+            echo '- '.str_pad($triple->subject, 80).' '.str_pad($triple->predicate, 80).' '.str_pad($triple->object, 80).PHP_EOL;
+        }
+        
+    }
+
+    public function applyTo(core_kernel_persistence_smoothsql_SmoothModel $smoothModel)
+    {
+        $rdf = $smoothModel->getRdfInterface();
+        foreach ($this->toRemove as $triple) {
+            $rdf->remove($triple);
+        }
+        foreach ($this->toAdd as $triple) {
+            $rdf->add($triple);
+        }
+    }
+}
