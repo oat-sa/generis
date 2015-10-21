c1af1ce02da13b786b2b35374002071689fefe66
Ivan Klimchuk <klimchuk@1pt.com>
2015-10-21 11:26:16 +0300

    Refactored SingleFileApender and add reducing options

--- common/log/class.SingleFileAppender.php
+++ common/log/class.SingleFileAppender.php
@@ -22,77 +22,81 @@
  */
 
 /**
- * Basic Appender that writes into a singel file
- * If the file exceeds maxFileSize the forst half is truncated
+ * Basic Appender that writes into a single file
+ * If the file exceeds maxFileSize the first half is truncated
  *
  * @access public
  * @author Joel Bout, <joel.bout@tudor.lu>
  * @package generis
- 
  */
-class common_log_SingleFileAppender
-    extends common_log_BaseAppender
+class common_log_SingleFileAppender extends common_log_BaseAppender
 {
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
     /**
-     * Short description of attribute filename
+     * Name of file with log entries
      *
      * @access protected
      * @var string
      */
     protected $filename = '';
+
+	/**
+	 * Format for each log line
+	 *
+	 * %d datestring
+	 * %m description(message)
+	 * %s severity
+	 * %b backtrace
+	 * %r request
+	 * %f file from which the log was called
+	 * %l line from which the log was called
+	 * %t timestamp
+	 * %u user
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $format = '%d [%s] \'%m\' %f %l';
+
+	/**
+	 * Prefix for each log line
+	 *
+	 * @var string
+	 */
+	protected $prefix = '';
+
+	/**
+	 * Maximum size of the logfile in bytes
+	 *
+	 * @access protected
+	 * @var int
+	 */
+	protected $maxFileSize = 1048576;
+
+	/**
+	 * Ratio value that using for reducing logfile when size of it reach max value
+	 *
+	 * @var float
+	 */
+	protected $reduceRatio = 0.5;
     
     /**
-     * Short description of attribute filehandle
+     * File descriptor for R/W operations
      *
      * @access protected
      * @var resource
      */
-    protected $filehandle = null;
-
-    /**
-     * %d datestring
-     * %m description(message)
-     * %s severity
-     * %b backtrace
-     * %r request
-     * %f file from which the log was called
-     * %l line from which the log was called
-     * %t timestamp
-     * %u user
-     *
-     * @access protected
-     * @var string
-     */
-    protected $format = '%d [%s] \'%m\' %f %l';
-
-    /**
-     * maximum size of the logfile in bytes
-     *
-     * @access protected
-     * @var int
-     */
-    protected $maxFileSize = 1048576;
-
-    // --- OPERATIONS ---
+    protected $filehandler = null;
 
     /**
      * Short description of method init
      *
      * @access public
      * @author Joel Bout, <joel.bout@tudor.lu>
-     * @param  array configuration
+     * @param  array $configuration
      * @return boolean
      */
     public function init($configuration)
     {
-        $returnValue = (bool) false;
-
-        
     	if (isset($configuration['file'])) {
     		$this->filename = $configuration['file'];
     	}
@@ -105,23 +109,23 @@ class common_log_SingleFileAppender
     		$this->prefix = $configuration['prefix'];
     	}
     	
-    	if (isset($configuration['max_file_size'])) {
-    		$this->maxFileSize = $configuration['max_file_size'];
+    	if (isset($configuration['reducing']['maxsize'])) {
+    		$this->maxFileSize = $configuration['reducing']['maxsize'];
     	}
-    	
-    	if (!empty($this->filename)){
-    		$returnValue = parent::init($configuration);
-        }
-    	else{
-    		$returnValue = false;
-        }
-        
 
-        return (bool) $returnValue;
+		if (isset($configuration['reducing']['ratio'])
+			&& $configuration['reducing']['ratio'] < 1
+		) {
+			$this->reduceRatio = $configuration['reducing']['ratio'];
+		}
+
+		return !empty($this->filename)
+			? parent::init($configuration)
+			: false;
     }
 
     /**
-     * initialisez the logfile, and checks whenever the file require prunning
+     * Initialises the logfile, and checks whenever the file require pruning
      *
      * @access protected
      * @author Joel Bout, <joel.bout@tudor.lu>
@@ -129,20 +133,20 @@ class common_log_SingleFileAppender
      */
     protected function initFile()
     {
-        
-        if ($this->maxFileSize > 0 && file_exists($this->filename) && filesize($this->filename) >= $this->maxFileSize) {
-        	
+        if ($this->maxFileSize > 0
+			&& file_exists($this->filename)
+			&& filesize($this->filename) >= $this->maxFileSize
+		) {
         	// need to reduce the file size
         	$file = file($this->filename);
-        	$file = array_splice($file, count($file) / 2);
-        	$this->filehandle = @fopen($this->filename, 'w');
+        	$file = array_splice($file, count($file) * $this->reduceRatio);
+        	$this->filehandler = @fopen($this->filename, 'w');
         	foreach ($file as $line) {
-        		@fwrite($this->filehandle, $line);
+        		@fwrite($this->filehandler, $line);
         	}
         } else {
-    		$this->filehandle = @fopen($this->filename, 'a');
+    		$this->filehandler = @fopen($this->filename, 'a');
         }
-        
     }
 
     /**
@@ -150,37 +154,35 @@ class common_log_SingleFileAppender
      *
      * @access public
      * @author Joel Bout, <joel.bout@tudor.lu>
-     * @param  Item item
+     * @param common_log_Item $item
      * @return mixed
      */
-    public function dolog( common_log_Item $item)
+    public function dolog(common_log_Item $item)
     {
-        
-    	if (is_null($this->filehandle)) {
+    	if (is_null($this->filehandler)) {
     		$this->initFile();
     	}
     	
-    	if ($this->filehandle !== false) {
+    	if ($this->filehandler !== false) {
 	    	$map = array(
-	    			'%d' => date('Y-m-d H:i:s',$item->getDateTime()),
-	    			'%m' => $item->getDescription(),
-	    			'%p' => $this->prefix,
-	    			'%s' => $item->getSeverityDescriptionString(),
-	    			'%t' => $item->getDateTime(),
-	    			'%r' => $item->getRequest(),
-	    			'%f' => $item->getCallerFile(),
-	    			'%l' => $item->getCallerLine()
+				'%d' => date('Y-m-d H:i:s',$item->getDateTime()),
+				'%m' => $item->getDescription(),
+				'%p' => $this->prefix,
+				'%s' => $item->getSeverityDescriptionString(),
+				'%t' => $item->getDateTime(),
+				'%r' => $item->getRequest(),
+				'%f' => $item->getCallerFile(),
+				'%l' => $item->getCallerLine()
 	    	);
 	    	
 	    	if (strpos($this->format, '%b')) {
 	    		$map['%b'] = 'Backtrace not yet supported';
 	    	}
-	    	
-	    	$str = strtr($this->format, $map).PHP_EOL;
-	    	
-	    	@fwrite($this->filehandle, $str);
+
+			$str = strtr($this->format, $map) . PHP_EOL;
+
+			@fwrite($this->filehandler, $str);
     	}
-        
     }
 
     /**
@@ -192,11 +194,8 @@ class common_log_SingleFileAppender
      */
     public function __destruct()
     {
-        
-    	if (!is_null($this->filehandle) && $this->filehandle !== false) {
-    		@fclose($this->filehandle);
+		if (!is_null($this->filehandler) && $this->filehandler !== false) {
+    		@fclose($this->filehandler);
     	}
-        
     }
-
 }
--- config/default/log.conf.php
+++ config/default/log.conf.php
@@ -24,39 +24,41 @@
  *
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @package generis
- 
+ *
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
 
-#	 trace_level 	= 0;
-#	 debug_level 	= 1;
-#	 info_level 	= 2;
-#	 warning_level 	= 3;
-#	 error_level	= 4;
-#	 fatal_level 	= 5;
-$GLOBALS['COMMON_LOGGER_CONFIG'] = array(
-/*
- array(
- 		'class'			=> 'SingleFileAppender',
- 		'threshold'		=> 4 ,
- 		'file'			=> dirname(__FILE__).'/../../log/error.txt',
- 		'format'		=> '%m'
- ),
-array(
-		'class'			=> 'ArchiveFileAppender',
-		'mask'			=> 62 , // 111110
-		'tags'			=> array('GENERIS', 'TAO')
-		'file'			=> '/var/log/tao/debug.txt',
-		'directory'		=> '/var/log/tao/',
-		'max_file_size'	=> 10000000
-),
-		array(
-				'class'			=> 'UDPAppender',
-				'host'			=> '127.0.0.1',
-				'port'			=> 5775,
-				'threshold'		=> 1
-		)
-		/**/
-);
+# threshold variants
+# trace_level   = 0;
+# debug_level   = 1;
+# info_level    = 2;
+# warning_level = 3;
+# error_level   = 4;
+# fatal_level   = 5;
 
-return $GLOBALS['COMMON_LOGGER_CONFIG'];
+return array(
+//    array(
+//        'class' => 'SingleFileAppender',
+//        'threshold' => 4,
+//        'reducing' => array(
+//            'maxsize' => 1048576, // in bytes
+//            'ratio' => .5
+//        ),
+//        'file' => dirname(__FILE__).'/../../log/error.txt',
+//        'format' => '%m'
+//    ),
+//    array(
+//        'class' => 'ArchiveFileAppender',
+//        'mask' => 62, // 111110
+//        'tags' => array('GENERIS', 'TAO'),
+//        'file' => '/var/log/tao/debug.txt',
+//        'directory' => '/var/log/tao/',
+//        'max_file_size' => 10000000
+//    ),
+//    array(
+//        'class' => 'UDPAppender',
+//        'host' => '127.0.0.1',
+//        'port' => 5775,
+//        'threshold' => 1
+//    )
+);
