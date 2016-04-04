306cf88227f147a7244ef5dc82373e63a5c7d619
Lionel Lecaque <lionel@taotesting.com>
2016-04-04 16:49:58 +0000

    bimp version to 2.16.2

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -187,7 +187,7 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('2.12.0');
         }
         
-        $this->skip('2.12.0', '2.16.1');
+        $this->skip('2.12.0', '2.16.2');
     }
     
     private function getReadableModelIds() {
