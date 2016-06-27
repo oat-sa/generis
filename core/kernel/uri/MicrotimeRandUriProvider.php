7510257317a0477037aa7766ecb6393a998f9dad
Joel Bout <joel@taotesting.com>
2016-06-27 10:14:56 +0200

    migrated MicrotimeRandUriProvider

--- core/kernel/uri/MicrotimeRandUriProvider.php
+++ core/kernel/uri/MicrotimeRandUriProvider.php
@@ -17,10 +17,11 @@
  * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
  *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 
+ *               2013      (update and modification) Open Assessment Technologies SA;
  */
+namespace oat\generis\model\kernel\uri;
 
-
+use oat\oatbox\service\ConfigurableService;
 /**
  * UriProvider implementation based on PHP microtime and rand().
  *
@@ -29,16 +30,25 @@
  * @package generis
  
  */
-class common_uri_MicrotimeRandUriProvider
-    implements common_uri_UriProvider
+class MicrotimeRandUriProvider extends ConfigurableService
+    implements \common_uri_UriProvider
 {
+    const OPTION_PERSISTENCE = 'persistence';
+    
+    const OPTION_NAMESPACE = 'namespace';
     // --- ASSOCIATIONS ---
-
-
+    
     // --- ATTRIBUTES ---
-
+    
     // --- OPERATIONS ---
-
+    
+    /**
+     * @return common_persistence_SqlPersistence
+     */
+    public function getPersistence() {
+        return \common_persistence_SqlPersistence::getPersistence($this->getOption(self::OPTION_PERSISTENCE));
+    }
+    
     /**
      * Generates a URI based on the value of PHP microtime() and rand().
      *
@@ -48,31 +58,19 @@ class common_uri_MicrotimeRandUriProvider
      */
     public function provide()
     {
-        $returnValue = (string) '';
-
         
-        $modelUri = common_ext_NamespaceManager::singleton()->getLocalNamespace()->getUri();
-		$dbWrapper = core_kernel_classes_DbWrapper::singleton();
-		$uriExist = false;
-		do{
-			list($usec, $sec) = explode(" ", microtime());
-        	$uri = $modelUri .'i'. (str_replace(".","",$sec."".$usec)) . rand(0, 1000);
-			$sqlResult = $dbWrapper->query("SELECT COUNT(subject) AS num FROM statements WHERE subject = '".$uri."'"
-			);
-			if ($row = $sqlResult->fetch()){
-				$found = (int)$row['num'];
-				if($found > 0){
-					$uriExist = true;
-				}
-				
-				$sqlResult->closeCursor();
-			}
-		}while($uriExist);
-		
-		$returnValue = $uri;
+        $uriExist = false;
+        do {
+            list($usec, $sec) = explode(" ", microtime());
+            $uri = $this->getOption(self::OPTION_NAMESPACE) . 'i' . (str_replace(".", "", $sec . "" . $usec)) . rand(0, 1000);
+            $sqlResult = $this->getPersistence()->query("SELECT COUNT(subject) AS num FROM statements WHERE subject = '" . $uri . "'");
+            if ($row = $sqlResult->fetch()) {
+                $uriExist = $row['num'] > 0;
+                $sqlResult->closeCursor();
+            }
+        } while ($uriExist);
         
-
-        return (string) $returnValue;
+        return (string) $uri;
     }
 
 }
