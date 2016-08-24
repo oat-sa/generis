1bac7e21f3df471381d6eafad0b3cbe4f07b6a1e
christophe garcia <christophe.garcia@taotesting.com>
2016-08-24 16:56:39 +0200

    fix updater script

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -236,13 +236,13 @@ class Updater extends \common_ext_ExtensionUpdater {
                         'search.query.criterion' => '\\oat\\search\\QueryCriterion',
                         'search.driver.postgres' => '\\oat\\search\\DbSql\\Driver\\PostgreSQL',
                         'search.driver.mysql' => '\\oat\\search\\DbSql\\Driver\\MySQL',
-                        'search.driver.tao' => ' \\oat\\generis\\model\\persistence\\smoothsql\\search\\driver\\TaoSearchDriver',
+                        'search.driver.tao' => '\\oat\\generis\\model\\kernel\\persistence\\smoothsql\\search\\driver\\TaoSearchDriver',
                         'search.tao.serialyser' => '\\oat\\search\\DbSql\\TaoRdf\\UnionQuerySerialyser',
                         'search.factory.query' => '\\oat\\search\\factory\\QueryFactory',
                         'search.factory.builder' => '\\oat\\search\\factory\\QueryBuilderFactory',
                         'search.factory.criterion' => '\\oat\\search\\factory\\QueryCriterionFactory',
-                        'search.tao.gateway' => ' \\oat\\generis\\model\\persistence\\smoothsql\\search\\GateWay',
-                        'search.tao.result' => ' \\oat\\generis\\model\\persistence\\smoothsql\\search\\TaoResultSet'
+                        'search.tao.gateway' => '\\oat\\generis\\model\\kernel\\persistence\\smoothsql\\search\\GateWay',
+                        'search.tao.result' => '\\oat\\generis\\model\\kernel\\persistence\\smoothsql\\search\\TaoResultSet'
                     ),
                     'abstract_factories' => array(
                         '\\oat\\search\\Command\\OperatorAbstractfactory'
