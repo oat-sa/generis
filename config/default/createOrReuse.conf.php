3ae1140f3954d60f02a4d08acf4abc205bb02102
christophe garcia <christophe.garcia@taotesting.com>
2016-10-13 11:04:52 +0200

    add default configuration

---
+++ config/default/createOrReuse.conf.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default config header
+ *
+ * To replace this add a file /home/christophe/Projects/tao/generis/config/header/createOrReuse.conf.php
+ */
+
+return new oat\generis\model\Resource\CreateOrReuseService();
