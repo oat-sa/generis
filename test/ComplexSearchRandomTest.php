67c9de42ecdb82c8ecd3971543cc82f24ddfe274
= <jerome@taotesting.com>
2016-10-14 10:36:57 +0200

    Put back original require.

---
+++ test/ComplexSearchRandomTest.php
@@ -0,0 +1,39 @@
+<?php
+/*  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
+ *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
+ * 
+ */
+error_reporting(E_ALL);
+
+use oat\generis\test\GenerisPhpUnitTestRunner;
+
+
+class ComplexSearchRandomTest extends GenerisPhpUnitTestRunner {
+    
+    public function testRandomize()
+    {
+        $search = oat\oatbox\service\ServiceManager::getServiceManager()->get(oat\generis\model\kernel\persistence\smoothsql\search\ComplexSearchService::SERVICE_ID);
+        $queryBuilder = $search->query();
+        $query = $search->searchType($queryBuilder , 'http://www.tao.lu/Ontologies/TAOSubject.rdf#Subject', true);
+        $queryBuilder->setCriteria($query);
+        $queryBuilder->setRandom()->setLimit(2);
+        $result = $search->getGateway()->search($queryBuilder);
+        
+        var_dump($result);
+    }
+}
