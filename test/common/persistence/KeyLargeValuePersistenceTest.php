df7c86c7e3450a2aecde4bf2fa21c88506c15c66
siwane <camille@taotesting.com>
2017-06-08 14:48:09 +0000

    Add advkv persistence for large value

--- test/common/persistence/KeyLargeValuePersistenceTest.php
+++ test/common/persistence/KeyLargeValuePersistenceTest.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ */
 
 namespace oat\generis\test\common\persistence;
 
@@ -12,9 +29,10 @@ class KeyLargeValuePersistenceTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->largeValuePersistence = new \common_persistence_KeyLargeValuePersistence(
-            array(),
-            new \common_persistence_InMemoryKvDriver(),
-            100
+            array(
+                \common_persistence_KeyLargeValuePersistence::VALUE_MAX_WIDTH => 100
+            ),
+            new \common_persistence_InMemoryAdvKvDriver()
         );
     }
 
@@ -41,6 +59,7 @@ class KeyLargeValuePersistenceTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($this->largeValuePersistence->exists('test'));
         $this->largeValuePersistence->set('test', $bigValue);
         $this->assertTrue($this->largeValuePersistence->exists('test'));
+        $this->assertEquals($bigValue, $this->largeValuePersistence->get('test'));
         $this->assertTrue($this->largeValuePersistence->del('test'));
         $this->assertFalse($this->largeValuePersistence->exists('test'));
         $this->assertEmpty($this->largeValuePersistence->get('test'));
@@ -62,4 +81,19 @@ class KeyLargeValuePersistenceTest extends \PHPUnit_Framework_TestCase
         $this->assertEmpty($this->largeValuePersistence->get('test'));
     }
 
+    public function testMapMapControl()
+    {
+        $this->largeValuePersistence = new \common_persistence_KeyLargeValuePersistence(
+            array(
+                \common_persistence_KeyLargeValuePersistence::VALUE_MAX_WIDTH => 100,
+                \common_persistence_KeyLargeValuePersistence::MAP_IDENTIFIER => 'iamamap',
+                \common_persistence_KeyLargeValuePersistence::START_MAP_DELIMITER => 'mapbegin',
+                \common_persistence_KeyLargeValuePersistence::END_MAP_DELIMITER => 'mapend',
+
+            ),
+            new \common_persistence_InMemoryAdvKvDriver()
+        );
+
+        $this->testDelExistsLarge();
+    }
 }
