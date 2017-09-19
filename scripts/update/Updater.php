bed079011df74ce7830c5ae86ba6b3d35bf0c353
siwane <camille@taotesting.com>
2017-09-19 14:35:42 +0000

    Bump to version 4.2.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -390,7 +390,7 @@ class Updater extends common_ext_ExtensionUpdater {
             $this->setVersion('3.35.2');
         }
 
-        $this->skip('3.35.2', '4.1.3');
+        $this->skip('3.35.2', '4.2.0');
     }
     
     private function getReadableModelIds() {
