67fb5559a57b44d0d8ed852b5e04ae75ad009e1f
siwane <camille@taotesting.com>
2017-09-19 11:44:17 +0000

    Bump to version 4.2.0

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'Generis Core',
     'description' => 'Core extension, provide the low level framework and an API to manage ontologies',
     'license' => 'GPL-2.0',
-    'version' => '4.3.0',
+    'version' => '4.2.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(),
     'models' => array(
