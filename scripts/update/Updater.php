6911948709165776e8f01552177512a2cde3b4fb
siwane <camille@taotesting.com>
2018-02-09 15:44:12 +0100

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -448,6 +448,7 @@ class Updater extends common_ext_ExtensionUpdater {
             $this->setVersion('6.9.0');
         }
 
+        $this->skip('6.9.0', '6.10.0');
     }
 
     private function getReadableModelIds() {
