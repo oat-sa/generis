a38eb7c11b6e413359751a4eee729a64a9d69dbc
Sergii <sergii@taotesting.com>
2018-03-01 11:42:36 +0100

    addTo modelId fixed

--- common/ext/class.GenerisInstaller.php
+++ common/ext/class.GenerisInstaller.php
@@ -1,24 +1,26 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- * 				 2013-2014 (update and modification) Open Assessment Technologies SA;
- * 
+ *               2013-2014 (update and modification) Open Assessment Technologies SA;
+ *
  */
+
+use core_kernel_persistence_smoothsql_SmoothModel as SmoothModel;
 use oat\generis\model\data\ModelManager;
 use oat\generis\model\kernel\persistence\smoothsql\search\ComplexSearchService;
 use oat\oatbox\service\ServiceManager;
@@ -29,7 +31,7 @@ use oat\oatbox\service\ServiceManager;
  * @access public
  * @author Joel Bout, <joel.bout@tudor.lu>
  * @package generis
- *         
+ *
  */
 class common_ext_GenerisInstaller extends common_ext_ExtensionInstaller
 {
@@ -52,19 +54,25 @@ class common_ext_GenerisInstaller extends common_ext_ExtensionInstaller
     public function install()
     {
         if ($this->extension->getId() != 'generis') {
-            throw new common_ext_ExtensionException('Tried to install "' . $this->extension->getId() . '" extension using the GenerisInstaller');
+            throw new common_ext_ExtensionException(
+                'Tried to install "' . $this->extension->getId() . '" extension using the GenerisInstaller'
+            );
         }
- 
+
         $this->installLoadDefaultConfig();
 
         $modelId = $this->newModeIdFactory->create();
-        
-        $model = new \core_kernel_persistence_smoothsql_SmoothModel(array(
-            \core_kernel_persistence_smoothsql_SmoothModel::OPTION_PERSISTENCE => 'default',
-            \core_kernel_persistence_smoothsql_SmoothModel::OPTION_READABLE_MODELS => array($modelId),
-            \core_kernel_persistence_smoothsql_SmoothModel::OPTION_WRITEABLE_MODELS => array($modelId),
-            \core_kernel_persistence_smoothsql_SmoothModel::OPTION_NEW_TRIPLE_MODEL => $modelId,
-            \core_kernel_persistence_smoothsql_SmoothModel::OPTION_SEARCH_SERVICE => ComplexSearchService::SERVICE_ID));
+        $userSpaceTripleModelId = 1;
+
+        $model = new SmoothModel(
+            [
+                SmoothModel::OPTION_PERSISTENCE      => 'default',
+                SmoothModel::OPTION_READABLE_MODELS  => [$modelId, $userSpaceTripleModelId],
+                SmoothModel::OPTION_WRITEABLE_MODELS => [$userSpaceTripleModelId],
+                SmoothModel::OPTION_NEW_TRIPLE_MODEL => $userSpaceTripleModelId,
+                SmoothModel::OPTION_SEARCH_SERVICE   => ComplexSearchService::SERVICE_ID
+            ]
+        );
         $model->setServiceLocator(ServiceManager::getServiceManager());
         ModelManager::setModel($model);
 
@@ -72,9 +80,9 @@ class common_ext_GenerisInstaller extends common_ext_ExtensionInstaller
         // $this->installLocalData();
         // $this->installModuleModel();
         $this->installRegisterExt($modelId);
-        
+
         common_cache_FileCache::singleton()->purge();
-        
+
         $this->log('d', 'Installing custom script for extension ' . $this->extension->getId());
         $this->installCustomScript();
     }
