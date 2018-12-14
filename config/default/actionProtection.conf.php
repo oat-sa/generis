cdfcdefb69acbf62ed0bbcd5f485a1d15adbbce2
Martijn Swinkels <martijn@taotesting.com>
2018-12-14 11:39:00 +0100

    Added default config

---
+++ config/default/actionProtection.conf.php
@@ -0,0 +1,7 @@
+<?php
+
+return new oat\oatbox\action\ActionProtector(array(
+    'frameSourceWhitelist' => array(
+        'self'
+    )
+));
---
+++ config/header/actionProtection.conf.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Configuration of the action protection service.
+ * This service can be used to add protection to certain actions, for example by setting CSP headers
+ * @see \oat\oatbox\action\ActionProtector
+ */
