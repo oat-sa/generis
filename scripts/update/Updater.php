df9e2f00a7476be0b64ff35c919a01a21092e953
siwane <camille@taotesting.com>
2019-02-26 14:07:16 +0100

    Merge develop

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -361,6 +361,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('8.0.0');
         }
 
-        $this->skip('8.0.0', '8.4.0');
+        $this->skip('8.0.0', '8.5.0');
     }
 }
