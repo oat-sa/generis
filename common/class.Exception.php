e93f2e10d5bb1c50c073d845e34c4145ccbc8f61
Sergii <sergii@taotesting.com>
2019-03-06 09:32:24 +0100

    useless constructor removed

--- common/class.Exception.php
+++ common/class.Exception.php
@@ -31,11 +31,6 @@
  */
 class common_Exception extends Exception
 {
-    public function __construct($message = null, $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
     public function __toString()
     {
         return sprintf(
