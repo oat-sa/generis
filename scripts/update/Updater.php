6325e29c0d6a6ee3086356af9e8952c32e7756a9
Aleh Hutnikau <hutnikau@1pt.com>
2019-05-23 15:58:18 +0300

    fix update to v11.2.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -402,7 +402,7 @@ class Updater extends common_ext_ExtensionUpdater
 //            ]);
 //            $this->getServiceManager()->register(LockService::SERVICE_ID, $service);
 //            $service->install();
-//            $this->setVersion('11.2.0');
+            $this->setVersion('11.2.0');
         }
 
         $this->skip('11.2.0', '11.2.1');
