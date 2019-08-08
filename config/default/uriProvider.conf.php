94477c4f578f13e734ff7761c06f18c119b08399
Joel Bout <joel@taotesting.com>
2019-08-08 11:01:28 +0200

    Cleanup uri Provider config

--- config/default/uriProvider.conf.php
+++ config/default/uriProvider.conf.php
@@ -9,7 +9,3 @@ use oat\generis\model\kernel\uri\Bin2HexUriProvider;
 return new Bin2HexUriProvider([
     Bin2HexUriProvider::OPTION_NAMESPACE => LOCAL_NAMESPACE.'#'
 ]);
-return new core_kernel_uri_DatabaseSerialUriProvider(array(
-    core_kernel_uri_DatabaseSerialUriProvider::OPTION_PERSISTENCE => 'default',
-    core_kernel_uri_DatabaseSerialUriProvider::OPTION_NAMESPACE => LOCAL_NAMESPACE.'#'
-));
