1bc8995b6aa377453d25e6ca46d813befd0f6987
Joel Bout <joel@taotesting.com>
2019-08-26 16:35:43 +0200

    Multiple formatting/styling

--- common/persistence/PersistenceManager.php
+++ common/persistence/PersistenceManager.php
@@ -15,42 +15,44 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
  */
 namespace oat\generis\persistence;
 
 use oat\oatbox\service\ConfigurableService;
+
 /**
- * A persistencefactory
+ * The PersistenceManager is responsible for initializing all persistences
  *
- * @author Lionel Lecaque  <lionel@taotesting.com>
+ * @author Lionel Lecaque <lionel@taotesting.com>
  * @license GPLv2
- * @package generis
- *
  */
 class PersistenceManager extends ConfigurableService
 {
+
     const SERVICE_ID = 'generis/persistences';
 
     const OPTION_PERSISTENCES = 'persistences';
 
     /**
+     * Mapping of drivers to implementations.
+     * All SQL drivers except 'dbal' are deprecated
      * @var array
      */
-    private static $driverMap = array(
+    const DRIVER_MAP = [
         'dbal' => 'common_persistence_sql_dbal_Driver',
-        'dbal_pdo_mysql'  => 'common_persistence_sql_dbal_Driver',
+        'dbal_pdo_mysql' => 'common_persistence_sql_dbal_Driver',
         'dbal_pdo_sqlite' => 'common_persistence_sql_dbal_Driver',
-        'dbal_pdo_pgsql'  => 'common_persistence_sql_dbal_Driver',
-        'dbal_pdo_ibm'    => 'common_persistence_sql_dbal_Driver',
-        'phpredis'   => 'common_persistence_PhpRedisDriver',
-        'phpfile'    => 'common_persistence_PhpFileDriver',
+        'dbal_pdo_pgsql' => 'common_persistence_sql_dbal_Driver',
+        'dbal_pdo_ibm' => 'common_persistence_sql_dbal_Driver',
+        'phpredis' => 'common_persistence_PhpRedisDriver',
+        'phpfile' => 'common_persistence_PhpFileDriver',
         'SqlKvWrapper' => 'common_persistence_SqlKvDriver',
         'no_storage' => 'common_persistence_InMemoryKvDriver',
         'no_storage_adv' => 'common_persistence_InMemoryAdvKvDriver'
-    );
+    ];
 
     /**
+     *
      * @var array
      */
     private $persistences = array();
@@ -71,7 +73,7 @@ class PersistenceManager extends ConfigurableService
      * Registers a new persistence.
      *
      * @param string $persistenceId
-     * @param array  $persistenceConf
+     * @param array $persistenceConf
      */
     public function registerPersistence($persistenceId, array $persistenceConf)
     {
@@ -79,7 +81,7 @@ class PersistenceManager extends ConfigurableService
         if (strpos($persistenceConf['driver'], 'pdo_') === 0) {
             $persistenceConf = [
                 'driver' => 'dbal',
-                'connection' => $persistenceConf,
+                'connection' => $persistenceConf
             ];
         }
 
@@ -93,39 +95,38 @@ class PersistenceManager extends ConfigurableService
     }
 
     /**
+     *
      * @return \common_persistence_Persistence
      */
     public function getPersistenceById($persistenceId)
     {
-        if(!isset($this->persistences[$persistenceId])) {
+        if (! isset($this->persistences[$persistenceId])) {
             $this->persistences[$persistenceId] = $this->createPersistence($persistenceId);
         }
         return $this->persistences[$persistenceId];
     }
 
     /**
+     *
      * @param string $persistenceId
      * @throws \common_Exception
      * @return \common_persistence_Persistence
      */
-    private function createPersistence($persistenceId) {
+    private function createPersistence($persistenceId)
+    {
         $configs = $this->getOption(self::OPTION_PERSISTENCES);
-        if (isset($configs[$persistenceId])) {
-            $config = $configs[$persistenceId];
-            $driverStr = $config['driver'];
-            
-            $driverClassName = isset(self::$driverMap[$driverStr])
-                ? self::$driverMap[$driverStr]
-                : $driverStr;
-             
-            if (!class_exists($driverClassName)){
-                throw new \common_exception_Error('Driver '.$driverStr.' not found check your database configuration');
-            }
-            $driver = $this->propagate(new $driverClassName());
-            return $driver->connect($persistenceId, $config);
-        } else {
-            throw new \common_Exception('Persistence Configuration for persistence '.$persistenceId.' not found');
+        if (! isset($configs[$persistenceId])) {
+            throw new \common_Exception('Persistence Configuration for persistence ' . $persistenceId . ' not found');
         }
+        $config = $configs[$persistenceId];
+        $driverString = $config['driver'];
+
+        $driverClassName = isset(self::DRIVER_MAP[$driverString]) ? self::DRIVER_MAP[$driverString] : $driverString;
+
+        if (! class_exists($driverClassName)) {
+            throw new \common_exception_Error('Driver ' . $driverString . ' not found, check your database configuration');
+        }
+        $driver = $this->propagate(new $driverClassName());
+        return $driver->connect($persistenceId, $config);
     }
-    
 }
