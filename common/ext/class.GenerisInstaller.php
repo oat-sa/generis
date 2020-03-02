367db772c6ec2514a46f7f9865ea0c7330b2d56c
Joel Bout <joel@taotesting.com>
2020-03-02 16:19:04 +0100

    Removed deprecated class reference

--- common/ext/class.GenerisInstaller.php
+++ common/ext/class.GenerisInstaller.php
@@ -54,7 +54,7 @@ class common_ext_GenerisInstaller extends common_ext_ExtensionInstaller
         $this->installOntology();
         $this->installRegisterExt();
 
-        common_cache_FileCache::singleton()->purge();
+        $this->getServiceManager()->get(common_cache_Cache::SERVICE_ID)->purge();
         
         $this->log('d', 'Installing custom script for extension ' . $this->extension->getId());
         $this->installCustomScript();
