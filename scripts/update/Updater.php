23ddf4057345ffc5690085357d9b9498a1cb59f7
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-03-24 11:21:31 +0100

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -493,7 +493,7 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('12.12.0');
         }
 
-        $this->skip('12.12.0', '12.16.0');
+        $this->skip('12.12.0', '12.16.1');
 
     }
 }
