605863faf785ea71101e65a3d24f1dfdce9a825a
Joel Bout <joel@taotesting.com>
2020-04-02 14:45:46 +0200

    Bump v12.16.4

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return [
     'label' => 'Generis Core',
     'description' => 'Core extension, provide the low level framework and an API to manage ontologies',
     'license' => 'GPL-2.0',
-    'version' => '12.16.3',
+    'version' => '12.16.4',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [],
     'install' => [
