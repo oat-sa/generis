d5a6b5d8319fb5b777a4627cdb47a9302e6aca22
Joel Bout <joel@taotesting.com>
2020-06-19 12:46:01 +0200

    A comment to helper

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -61,6 +61,7 @@ use oat\oatbox\user\UserLanguageService;
 use oat\taoWorkspace\model\generis\WrapperModel;
 use Psr\Log\LoggerInterface;
 use oat\generis\model\kernel\persistence\file\FileIterator;
+use Traversable;
 
 /**
  * @author Joel Bout <joel@taotesting.com>
@@ -545,8 +546,15 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('12.28.1');
         }
     }
-    
-    protected function removeDuplicates(\Traversable $triples): void
+
+    /**
+     * Helper for the unfortunate scenario where we generated duplicate
+     * entries in the statement table.
+     *
+     * Fixes the duplicates by removing all matching triples and then
+     * readding them only once
+     */
+    protected function removeDuplicates(Traversable $triples): void
     {
         $rdfModel = $this->getServiceLocator()->get(Ontology::SERVICE_ID)->getRdfInterface();
         foreach ($triples as $triple) {
