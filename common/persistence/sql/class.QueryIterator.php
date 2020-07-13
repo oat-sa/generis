6b84e8a2cab5a410a390defa3a5ae9ff1cfebf90
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-07-13 23:43:51 +0300

    PDO type support PSR-12 limit customisation

--- common/persistence/sql/class.QueryIterator.php
+++ common/persistence/sql/class.QueryIterator.php
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2002-2008 (original work) 2014 Open Assessment Technologies SA
+ * Copyright (c) 2002-2020 (original work) 2014 Open Assessment Technologies SA
  *
  */
 
@@ -22,91 +22,70 @@
  * Iterator over all triples
  *
  * @author joel bout <joel@taotesting.com>
- * @package generis
  */
 class common_persistence_sql_QueryIterator implements Iterator
 {
-    const CACHE_SIZE = 100;
-    
+    private const CACHE_SIZE = 100;
+
     /**
      * @var common_persistence_SqlPersistence
      */
     private $persistence;
-    
-    /**
-     * Query to iterator over
-     *
-     * @var string
-     */
+
     private $query;
-    
-    /**
-     * Query parameters
-     *
-     * @var string
-     */
+
     private $params;
-    
+
     /**
      * Id of the current instance
      *
      * @var int
      */
-    private $currentResult = null;
+    private $currentResult;
 
     /**
      * Return statements of the last query
      *
      * @var array
      */
-    private $cache = null;
-    
-    /**
-     * Constructor of the iterator expecting the model ids
-     *
-     * @param array $modelIds
-     */
-    public function __construct(common_persistence_SqlPersistence $persistence, $query, $params = [])
-    {
+    private $cache;
+    private $types;
+    private $limit;
+
+    public function __construct(
+        common_persistence_SqlPersistence $persistence,
+        $query,
+        $params = [],
+        array $types = [],
+        int $limit = self::CACHE_SIZE
+    ) {
         $this->persistence = $persistence;
         $this->query = $query;
         $this->params = $params;
+        $this->types = $types;
+        $this->limit = $limit;
         $this->rewind();
     }
-    
-    /**
-     * (non-PHPdoc)
-     * @see Iterator::rewind()
-     */
-    function rewind()
+
+    public function rewind()
     {
         $this->load(0);
     }
-    
+
     /**
-     * (non-PHPdoc)
-     * @see Iterator::current()
      * @return core_kernel_classes_Triple
      */
-    function current()
+    public function current()
     {
         return $this->cache[$this->currentResult];
     }
-    
-    /**
-     * (non-PHPdoc)
-     * @see Iterator::key()
-     */
-    function key()
+
+    public function key()
     {
         return $this->currentResult;
     }
-    
-    /**
-     * (non-PHPdoc)
-     * @see Iterator::next()
-     */
-    function next()
+
+    public function next()
     {
         if ($this->valid()) {
             $last = $this->key();
@@ -116,16 +95,12 @@ class common_persistence_sql_QueryIterator implements Iterator
             }
         }
     }
-    
-    /**
-     * (non-PHPdoc)
-     * @see Iterator::valid()
-     */
-    function valid()
+
+    public function valid()
     {
         return !empty($this->cache);
     }
-    
+
     /**
      * Loads the next n results, starting with $offset
      *
@@ -133,9 +108,8 @@ class common_persistence_sql_QueryIterator implements Iterator
      */
     protected function load($offset)
     {
-
-        $query = $this->persistence->getPlatForm()->limitStatement($this->query, self::CACHE_SIZE, $offset);
-        $result = $this->persistence->query($query, $this->params);
+        $query = $this->persistence->getPlatForm()->limitStatement($this->query, $this->limit, $offset);
+        $result = $this->persistence->query($query, $this->params, $this->types);
 
         $this->cache = [];
         $pos = $offset;
