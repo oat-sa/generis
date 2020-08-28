4a886e5d64288da59c072d1eda055950862539d9
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-08-28 13:04:21 +0300

    Lib dependency version bumped

--- manifest.php
+++ manifest.php
@@ -32,7 +32,7 @@ return [
     'label' => 'Generis Core',
     'description' => 'Core extension, provide the low level framework and an API to manage ontologies',
     'license' => 'GPL-2.0',
-    'version' => '13.2.0',
+    'version' => '13.3.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [],
     'install' => [
