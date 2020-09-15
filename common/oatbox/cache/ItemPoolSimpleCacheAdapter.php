86d405f993738948c76d51b179bed44077920a8f
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-09-15 14:46:02 +0200

    removed noinspection annotation

--- common/oatbox/cache/ItemPoolSimpleCacheAdapter.php
+++ common/oatbox/cache/ItemPoolSimpleCacheAdapter.php
@@ -1,7 +1,5 @@
 <?php
 
-/** @noinspection MissingReturnTypeInspection */
-
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
