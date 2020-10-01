686490da48ee62bd1fbd9475bb71294e4f7e3d04
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-10-01 17:14:58 +0200

    debug constant as DI

--- config/default/GcpTokenCacheItemPool.conf.php
+++ config/default/GcpTokenCacheItemPool.conf.php
@@ -23,7 +23,7 @@ use oat\oatbox\cache\GcpTokenCacheItemPool;
 return new GcpTokenCacheItemPool(
     [
         GcpTokenCacheItemPool::OPTION_PERSISTENCE => 'gcpTokenKeyValue',
-        GcpTokenCacheItemPool::OPTION_ENABLE_DEBUG => false,
+        GcpTokenCacheItemPool::OPTION_ENABLE_DEBUG => defined('DEBUG_MODE') ? DEBUG_MODE : false,
         GcpTokenCacheItemPool::OPTION_DISABLE_WRITE => true,
         GcpTokenCacheItemPool::OPTION_TOKEN_CACHE_KEY => 'GCP-TOKEN-SANCTUARY:GOOGLE_AUTH_PHP_GCE',
     ]
