e054f6fdc9632cb5aa15b604a5016cc969122252
Andrey Niahrou <Andrei.Niahrou@1pt.com>
2021-03-05 11:52:22 +0300

    chore: removed versioning from manifest

--- manifest.php
+++ manifest.php
@@ -32,7 +32,6 @@ return [
     'label' => 'Generis Core',
     'description' => 'Core extension, provide the low level framework and an API to manage ontologies',
     'license' => 'GPL-2.0',
-    'version' => '13.15.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'install' => [
         'rdf' => [
