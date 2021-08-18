8fe223cf358a84ad623bf4a2ce115fd5d3403848
Ricardo Quintanilha <ricardo.quintanilha@taotesting.com>
2021-08-18 17:52:37 +0200

    feat: Allow interface language to be overwritten in the BasicSession

--- common/session/class.BasicSession.php
+++ common/session/class.BasicSession.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
@@ -142,6 +142,10 @@ class common_session_BasicSession implements common_session_Session, ServiceLoca
      */
     public function getInterfaceLanguage()
     {
+        if (PHPSession::singleton()->hasAttribute('overrideInterfaceLanguage')) {
+            return PHPSession::singleton()->getAttribute('overrideInterfaceLanguage');
+        }
+
         return $this->getServiceLocator()->get(UserLanguageServiceInterface::class)->getInterfaceLanguage($this->getUser());
     }
     
