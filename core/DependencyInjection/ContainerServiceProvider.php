2f853dc40d863db798ba563145b5fafb59865e48
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2021-12-17 06:49:23 +0100

    feat: add request/response to container and improve container::has method

--- core/DependencyInjection/ContainerServiceProvider.php
+++ core/DependencyInjection/ContainerServiceProvider.php
@@ -22,6 +22,10 @@ declare(strict_types=1);
 
 namespace oat\generis\model\DependencyInjection;
 
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 class ContainerServiceProvider implements ContainerServiceProviderInterface
@@ -29,6 +33,16 @@ class ContainerServiceProvider implements ContainerServiceProviderInterface
     public function __invoke(ContainerConfigurator $configurator): void
     {
         $services = $configurator->services();
+
+        /**
+         * @TODO This needs to be override in the scope of middleware task [ADF-731]
+         */
+        $services->set(ServerRequestInterface::class, ServerRequestInterface::class)
+            ->public()
+            ->factory(ServerRequest::class . '::fromGlobals');
+
+        $services->set(ResponseInterface::class, Response::class)
+            ->public();
 
         $services->set(LegacyServiceGateway::class, LegacyServiceGateway::class);
     }
