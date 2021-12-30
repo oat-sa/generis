f5c63a7e66d2b1872a41c444b0b946302ed82c9c
Héctor Arroyo <churruka@hangar18.cc>
2021-12-30 10:03:45 +0100

    ADF-842: Make UserLanguageService::getDefaultLanguage() configurable (#950)
    * feat: Allow setting lang for UserLanguageService::getInterfaceLanguage() via UserLanguageService.conf.php option default_language as interface_language
    
    * fix: Do not return DEFAULT_LANG for PROPERTY_USER_UILG
    
    Co-authored-by: Andrei Shapiro <59471572+shpran@users.noreply.github.com>
    Co-authored-by: Gabriel Felipe Soares <gabrielfs7@gmail.com>

--- core/kernel/users/class.GenerisUser.php
+++ core/kernel/users/class.GenerisUser.php
@@ -75,33 +75,56 @@ class core_kernel_users_GenerisUser extends common_user_User implements UserInte
         } elseif (!isset($this->cache[$property])) {
             $this->cache[$property] = $this->getUncached($property);
         }
+
         return $this->cache[$property];
     }
 
     private function getUncached($property)
     {
-        $value = [];
         switch ($property) {
             case GenerisRdf::PROPERTY_USER_DEFLG:
+                $lang = $this->findLanguage($property);
+
+                return $lang ?: [DEFAULT_LANG];
+
             case GenerisRdf::PROPERTY_USER_UILG:
-                $resource = $this->getUserResource()->getOnePropertyValue(new core_kernel_classes_Property($property));
-                if (!is_null($resource)) {
-                    if ($resource instanceof core_kernel_classes_Resource) {
-                        $value = (string)$resource->getUniquePropertyValue(new core_kernel_classes_Property(OntologyRdf::RDF_VALUE));
-                        return [$value];
-                    } else {
-                        common_Logger::w('Language ' . $resource . ' is not a resource');
-                        return [DEFAULT_LANG];
-                    }
-                } else {
-                    return [DEFAULT_LANG];
-                }
-                break;
+                return $this->findLanguage($property);
+
             default:
                 return $this->getUserResource()->getPropertyValues(new core_kernel_classes_Property($property));
         }
     }
-    
+
+    /**
+     * @return array|string[]
+     * @throws common_Exception
+     * @throws core_kernel_classes_EmptyProperty
+     * @throws core_kernel_persistence_Exception
+     */
+    private function findLanguage(string $propertyURI): array
+    {
+        $language = [];
+        $resource = $this->getUserResource()->getOnePropertyValue(
+            new core_kernel_classes_Property($propertyURI)
+        );
+
+        if (is_null($resource)) {
+            return $language;
+        }
+
+        if (!$resource instanceof core_kernel_classes_Resource) {
+            common_Logger::w("Language {$resource} is not a resource");
+
+            return $language;
+        }
+
+        return [
+           (string) $resource->getUniquePropertyValue(
+               new core_kernel_classes_Property(OntologyRdf::RDF_VALUE)
+           )
+        ];
+    }
+
     public function refresh()
     {
         $this->roles = false;
