e362d019a12b0ea2e7e7b8b211c642e3f9edbaa4
Camille Moyon <camille@taotesting.com>
2022-01-18 14:17:08 +0100

    feat: make resolver able to discover action namespace

--- common/oatbox/action/ActionService.php
+++ common/oatbox/action/ActionService.php
@@ -15,42 +15,50 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2015-2022 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
+declare(strict_types=1);
+
 namespace oat\oatbox\action;
 
+use common_ext_Extension;
+use helpers_PhpTools;
 use oat\oatbox\service\ConfigurableService;
 use oat\oatbox\cache\SimpleCache;
 use common_ext_ExtensionsManager;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use ReflectionClass;
+use RegexIterator;
+use Throwable;
 
 class ActionService extends ConfigurableService
 {
     const SERVICE_ID = 'generis/actionService';
-    
+
     static $blackList = ['\\oatbox\\composer\\ExtensionInstaller','\\oatbox\\composer\\ExtensionInstallerPlugin'];
-    
+
     /**
-     *
-     * @param string $actionIdentifier
-     * @return Action
+     * @throws ResolutionException
      */
-    public function resolve($actionIdentifier)
+    public function resolve($actionIdentifier): Action
     {
-        $action = null;
         if ($this->getServiceLocator()->has($actionIdentifier)) {
-            $action = $this->getServiceManager()->get($actionIdentifier);
+            $action = $this->getServiceLocator()->get($actionIdentifier);
         } elseif (class_exists($actionIdentifier) && is_subclass_of($actionIdentifier, Action::class)) {
             $action = new $actionIdentifier();
             $this->propagate($action);
         } else {
             throw new ResolutionException('Unknown action ' . $actionIdentifier);
         }
+
         return $action;
     }
-    
-    public function getAvailableActions()
+
+    public function getAvailableActions(): array
     {
         $actions = $this->getCache()->get(__FUNCTION__);
         if (is_null($actions)) {
@@ -63,19 +71,42 @@ class ActionService extends ConfigurableService
         }
         return $actions;
     }
-    
+
+    /**
+     * @throws ResolutionException
+     */
+    public function getActionExtension(Action $action): common_ext_Extension
+    {
+        $reflectionClass = new ReflectionClass($action);
+        $namespace = $reflectionClass->getNamespaceName();
+        $extensionId = explode('\\', $namespace)[1] ?? null;
+
+        try {
+            return $this->getServiceLocator()
+                ->get(common_ext_ExtensionsManager::SERVICE_ID)
+                ->getExtensionById($extensionId);
+        } catch (Throwable $exception) {
+            throw new ResolutionException(sprintf('Cannot get extension of action `%s`', $namespace));
+        }
+    }
+
+    public function getActionName(Action $action): string
+    {
+        return (new ReflectionClass($action))->getName();
+    }
+
     protected function getCache(): SimpleCache
     {
         return $this->getServiceLocator()->get(SimpleCache::SERVICE_ID);
     }
-    
-    protected function getActionsInDirectory($dir)
+
+    protected function getActionsInDirectory(string $dir): array
     {
         $classNames = [];
-        $recIt = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
-        $regexIt = new \RegexIterator($recIt, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH);
+        $recIt = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+        $regexIt = new RegexIterator($recIt, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
         foreach ($regexIt as $entry) {
-            $info = \helpers_PhpTools::getClassInfo($entry[0]);
+            $info = helpers_PhpTools::getClassInfo($entry[0]);
             $fullname = empty($info['ns'])
             ? $info['class']
             : $info['ns'] . '\\' . $info['class'];
