4c9354853cf70014972764ab43eac9229cccf8aa
Andrei Shapiro <shpr.andrew@gmail.com>
2022-03-04 11:46:21 +0000

    tests: use PhpUnit instead of Prophecy

--- test/TestCase.php
+++ test/TestCase.php
@@ -15,45 +15,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018-2021 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2018-2022 (original work) Open Assessment Technologies SA;
  */
+
+declare(strict_types=1);
 
 namespace oat\generis\test;
 
-use oat\oatbox\service\ServiceManager;
-use Prophecy\Argument;
-use Psr\Container\ContainerInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
 use PHPUnit\Framework\TestCase as UnitTestCase;
 
 abstract class TestCase extends UnitTestCase
 {
     use SqlMockTrait;
-
-    /**
-     * @param array $services
-     * @return ServiceLocatorInterface|ServiceManager
-     */
-    public function getServiceLocatorMock(array $services = [])
-    {
-        /** @var ContainerInterface $containerProphecy */
-        $containerProphecy = $this->prophesize(ContainerInterface::class);
-
-        /** @var ServiceManager $serviceLocatorProphecy */
-        $serviceLocatorProphecy = $this->prophesize(ServiceManager::class);
-        $serviceLocatorProphecy->getContainer()->willReturn($containerProphecy);
-
-        foreach ($services as $key => $service) {
-            $serviceLocatorProphecy->get($key)->willReturn($service);
-            $serviceLocatorProphecy->has($key)->willReturn(true);
-
-            $containerProphecy->get($key)->willReturn($service);
-            $containerProphecy->has($key)->willReturn(true);
-        }
-
-        $serviceLocatorProphecy->has(Argument::any())->willReturn(false);
-        $containerProphecy->has(Argument::any())->willReturn(false);
-
-        return $serviceLocatorProphecy->reveal();
-    }
+    use ServiceLocatorMockTrait;
 }
