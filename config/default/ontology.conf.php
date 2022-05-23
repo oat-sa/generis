8b29693cc3dc433d51d84a8857dfc6464f841130
Andrei Shapiro <shpr.andrew@gmail.com>
2022-05-23 21:32:28 +0000

    refactor: beautify default ontology config

--- config/default/ontology.conf.php
+++ config/default/ontology.conf.php
@@ -3,13 +3,15 @@
 use core_kernel_persistence_smoothsql_SmoothModel as SmoothModel;
 use oat\generis\model\kernel\persistence\smoothsql\search\ComplexSearchService;
 
-return new \core_kernel_persistence_smoothsql_SmoothModel([
+return new SmoothModel([
     SmoothModel::OPTION_PERSISTENCE => 'default',
-    SmoothModel::OPTION_READABLE_MODELS =>
-        [SmoothModel::DEFAULT_WRITABLE_MODEL, SmoothModel::DEFAULT_READ_ONLY_MODEL],
-    SmoothModel::OPTION_WRITEABLE_MODELS =>
-        [SmoothModel::DEFAULT_WRITABLE_MODEL],
-    SmoothModel::OPTION_NEW_TRIPLE_MODEL =>
+    SmoothModel::OPTION_READABLE_MODELS => [
         SmoothModel::DEFAULT_WRITABLE_MODEL,
-    SmoothModel::OPTION_SEARCH_SERVICE => ComplexSearchService::SERVICE_ID
+        SmoothModel::DEFAULT_READ_ONLY_MODEL,
+    ],
+    SmoothModel::OPTION_WRITEABLE_MODELS => [
+        SmoothModel::DEFAULT_WRITABLE_MODEL,
+    ],
+    SmoothModel::OPTION_NEW_TRIPLE_MODEL => SmoothModel::DEFAULT_WRITABLE_MODEL,
+    SmoothModel::OPTION_SEARCH_SERVICE => ComplexSearchService::SERVICE_ID,
 ]);
