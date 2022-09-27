f968b8525390cc8a2b208024547ea190f65e83f5
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2022-09-27 11:26:04 +0200

    chore: code style fixes

--- test/unit/oatbox/mutex/test_action.php
+++ test/unit/oatbox/mutex/test_action.php
@@ -5,29 +5,23 @@ use oat\oatbox\service\ServiceManager;
 use Symfony\Component\Lock\Store\FlockStore;
 use oat\oatbox\mutex\NoLockStorage;
 
+// phpcs:disable
 require __DIR__ . '/../../../../../vendor/autoload.php';
 
-/**
- * @param $class
- * @param $dir
- * @return LockService
- * @throws common_Exception
- */
+function getInstance($class, $dir): LockService
+{
+    $config = new \common_persistence_KeyValuePersistence([], new \common_persistence_InMemoryKvDriver());
+    $config->set(\common_persistence_Manager::SERVICE_ID, new \common_persistence_Manager());
+    $serviceManager = new ServiceManager($config);
+    $service = new LockService([
+        LockService::OPTION_PERSISTENCE_CLASS => $class,
+        LockService::OPTION_PERSISTENCE_OPTIONS => $dir
+    ]);
+    $service->setServiceLocator($serviceManager);
 
-if (!function_exists('getInstance')) {
-    function getInstance($class, $dir)
-    {
-        $config = new \common_persistence_KeyValuePersistence([], new \common_persistence_InMemoryKvDriver());
-        $config->set(\common_persistence_Manager::SERVICE_ID, new \common_persistence_Manager());
-        $serviceManager = new ServiceManager($config);
-        $service = new LockService([
-            LockService::OPTION_PERSISTENCE_CLASS => $class,
-            LockService::OPTION_PERSISTENCE_OPTIONS => $dir
-        ]);
-        $service->setServiceLocator($serviceManager);
-        return $service;
-    }
+    return $service;
 }
+// phpcs:enable
 
 array_shift($argv);
 $actionId = $argv[0];
