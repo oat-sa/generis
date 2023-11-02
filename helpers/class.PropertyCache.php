7d804f9fe6f1342a7c317de568f5c26afa93f282
Makar Sichevoi <makar.sichevoy@taotesting.com>
2023-11-02 11:06:17 +0200

    feat: decrease query count by cacheing some RDF property fields

--- helpers/class.PropertyCache.php
+++ helpers/class.PropertyCache.php
@@ -5,22 +5,22 @@ class helpers_PropertyCache
     /**
      * Clear property cached data
      */
-    public static function clearCachedValues(Iterator $triples): void
+    public static function clearCachedValuesByTriples(Iterator $triples): void
     {
         foreach ($triples as $triple) {
-            $property = new \core_kernel_classes_Property($triple->predicate);
-            $property->clearCachedValues();
+            $classProperty = new \core_kernel_classes_Property($triple->predicate);
+            $classProperty->clearCachedValues();
         }
     }
 
     /**
      * Warmup property cached data
      */
-    public static function warmupCachedValues(Iterator $triples): void
+    public static function warmupCachedValuesByProperties(array $properties): void
     {
-        foreach ($triples as $triple) {
-            $property = new \core_kernel_classes_Property($triple->predicate);
-            $property->warmupCachedValues();
+        foreach ($properties as $property) {
+            $classProperty = new \core_kernel_classes_Property($property);
+            $classProperty->warmupCachedValues();
         }
     }
 }
