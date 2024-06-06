59e3530ff4b78fb56c02b6bcec4e2166802a8718
Sultan <sulasagi@gmail.com>
2024-06-06 16:13:43 +0500

    feat: make translation with prefix

--- common/oatbox/user/UserLanguageService.php
+++ common/oatbox/user/UserLanguageService.php
@@ -15,19 +15,22 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018-2021 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2018-2024 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 namespace oat\oatbox\user;
 
 use oat\oatbox\service\ConfigurableService;
 use oat\generis\model\GenerisRdf;
+use oat\tao\helpers\Layout;
 
 class UserLanguageService extends ConfigurableService implements UserLanguageServiceInterface
 {
     public const OPTION_LOCK_DATA_LANGUAGE = 'lock_data_language';
     public const OPTION_AUTHORING_LANGUAGE = 'authoring_language';
     public const OPTION_INTERFACE_LANGUAGE = 'interface_language';
+
+    public const LANG_PREFIX = '-S';
 
     /** @var ?string */
     private $customInterfaceLanguage;
@@ -64,8 +67,15 @@ class UserLanguageService extends ConfigurableService implements UserLanguageSer
         }
 
         $lang = $user->getPropertyValues(GenerisRdf::PROPERTY_USER_UILG);
+        if (empty($lang)) {
+            $lang = $this->getOption(self::OPTION_INTERFACE_LANGUAGE, DEFAULT_LANG);
+        } else {
+            $lang = (string)current($lang);
+        }
 
-        return empty($lang) ? $this->getOption(self::OPTION_INTERFACE_LANGUAGE, DEFAULT_LANG) : (string)current($lang);
+        $lang = $this->checkPrefix($lang);
+
+        return $lang;
     }
 
     /**
@@ -88,5 +98,54 @@ class UserLanguageService extends ConfigurableService implements UserLanguageSer
     public function setCustomInterfaceLanguage(?string $customInterfaceLanguage): void
     {
         $this->customInterfaceLanguage = $customInterfaceLanguage;
+    }
+
+    /**
+     * Short description of method notContainPrefix
+     *
+     * @param string $language
+     * @access private
+     * @author Sultan Sagi, <sultan.sagiyev@taotesting.com>
+     * @return bool
+     */
+    private function notContainPrefix($language)
+    {
+        $pattern = '/' . self::LANG_PREFIX . '$/';
+
+        return Layout::isSolarDesignEnabled() && !preg_match($pattern, $language, $matches);
+    }
+
+    /**
+     * Short description of method addPrefix
+     *
+     * @param string $language
+     * @access private
+     * @author Sultan Sagi, <sultan.sagiyev@taotesting.com>
+     * @return string
+     */
+    private function addPrefix($language)
+    {
+        return $language . self::LANG_PREFIX;
+    }
+
+    /**
+     * Short description of method addPrefix
+     *
+     * @param string $language
+     * @access private
+     * @author Sultan Sagi, <sultan.sagiyev@taotesting.com>
+     * @return string
+     */
+    private function checkPrefix($language)
+    {
+        if ($this->notContainPrefix($language)) {
+            $localesDir = 'tao/views/locales';
+            $dir = dirname(__FILE__) . '/../../../../' . $localesDir . '/' . $this->addPrefix($language);
+            if (is_dir($dir)) {
+                $language = $this->addPrefix($language);
+            }
+        }
+
+        return $language;
     }
 }
