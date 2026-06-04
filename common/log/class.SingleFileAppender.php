47e49d01ac4e778f2f74cee738855839cee54148
pnal <nikita.pimenov@taotesting.com>
2026-06-04 17:17:51 +0400

    chore: remove string appender modification

--- common/log/class.SingleFileAppender.php
+++ common/log/class.SingleFileAppender.php
@@ -59,8 +59,6 @@ class common_log_SingleFileAppender extends common_log_BaseAppender
      * %t timestamp
      * %u user
      * %g tags (JSON)
-     * %traceId active OpenTelemetry trace id from PSR-3 context (when present)
-     * %spanId active OpenTelemetry span id from PSR-3 context (when present)
      *
      * @access protected
      * @var string
@@ -165,10 +163,6 @@ class common_log_SingleFileAppender extends common_log_BaseAppender
             $this->initFile();
         }
         if ($this->filehandle !== false) {
-            $tags = $item->getTags();
-            $traceId = is_array($tags) && isset($tags['traceId']) ? (string) $tags['traceId'] : '';
-            $spanId = is_array($tags) && isset($tags['spanId']) ? (string) $tags['spanId'] : '';
-
             $map = [
                 '%d' => gmdate('Y-m-d H:i:s', $item->getDateTime()),
                 '%m' => $item->getDescription(),
@@ -177,10 +171,8 @@ class common_log_SingleFileAppender extends common_log_BaseAppender
                 '%t' => $item->getDateTime(),
                 '%r' => $item->getRequest(),
                 '%f' => $item->getCallerFile(),
-                '%g' => json_encode($tags),
+                '%g' => json_encode($item->getTags()),
                 '%l' => $item->getCallerLine(),
-                '%traceId' => $traceId,
-                '%spanId' => $spanId,
             ];
             if (strpos($this->format, '%b')) {
                 $map['%b'] = 'Backtrace not yet supported';
